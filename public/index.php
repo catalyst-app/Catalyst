cc915c8da1ab42931ea0c85c4ce9cd8775ffa6a4
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-01-14 12:35:11 -0500

    Add GA back in

--- public/index.php
+++ public/index.php
@@ -305,12 +305,12 @@
 			</div>
 		</footer>
 	</body>
-	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-REDACTED-1"></script> -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112460506-1"></script>
 	<script>
-		// window.dataLayer = window.dataLayer || [];
-		// function gtag(){dataLayer.push(arguments);}
-		// gtag('js', new Date());
-		// gtag('config', 'UA-REDACTED-1');
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-112460506-1');
 
 		(function($){
 			$(function(){
