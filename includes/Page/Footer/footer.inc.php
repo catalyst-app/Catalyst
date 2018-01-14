41554d1b97197a1178419552fe2a9768a32d2375
Fauxil Fox <fauxil_fox@furmail.net>
2018-01-14 12:35:11 -0500

    Add GA back in

--- includes/Page/Footer/footer.inc.php
+++ includes/Page/Footer/footer.inc.php
@@ -12,11 +12,11 @@
 		</footer>
 	</body>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109915452-2"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112460506-1"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
 		function gtag(){dataLayer.push(arguments);}
 		gtag('js', new Date());
-		gtag('config', 'UA-109915452-2');
+		gtag('config', 'UA-112460506-1');
 	</script>
 </html>
