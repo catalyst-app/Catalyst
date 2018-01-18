f0c5403cca8f7ccf3e8e89395105cafa605afbfc
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-01-17 21:21:48 -0500

    Fix Response class error for vote handler

--- FeatureBoard/vote.php
+++ FeatureBoard/vote.php
@@ -6,6 +6,7 @@ define("REAL_ROOTDIR", "../");
 require_once REAL_ROOTDIR."includes/init.php";
 use \Catalyst\Database\FeatureBoard\Groups;
 use \Catalyst\Database\FeatureBoard\Item;
+use \Catalyst\Response;
 use \Catalyst\User\User;
 
 if (!isset($_POST["feature"]) || !preg_match('/^\d+$/', $_POST["feature"]) || User::isLoggedOut() || !in_array($_POST["vote"], ["YES","MAYBE","NO","IRRELEVANT"]) || Item::hasVoted((int)($_POST["feature"]), $_SESSION["user"]->getId()) || Item::get((int)($_POST["feature"]))["STATUS_KEY"] != "VOTE") {
