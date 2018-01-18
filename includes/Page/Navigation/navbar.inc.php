e0f69a54f1f5c8b44ef10f5588828cd2b03e7a0b
Fauxil Fox <fauxil_fox@furmail.net>
2018-01-17 20:10:32 -0500

    Use a more specific variable name in navbar file

--- includes/Page/Navigation/navbar.inc.php
+++ includes/Page/Navigation/navbar.inc.php
@@ -11,28 +11,28 @@
 							</i>
 						</a>
 						<ul class="right hide-on-med-and-down">
-<?php foreach (\Catalyst\Page\Navigation\Navbar::getNavbarItems(\Catalyst\User\User::getPermissionScope()) as $item): ?>
-<?php switch ($item[4]): ?>
+<?php foreach (\Catalyst\Page\Navigation\Navbar::getNavbarItems(\Catalyst\User\User::getPermissionScope()) as $navbarItem): ?>
+<?php switch ($navbarItem[4]): ?>
 <?php case \Catalyst\Page\Navigation\Navbar::NORMAL_LINK: ?>
-							<li<?= ($item[2] == PAGE_KEYWORD) ? ' class="active"' : "" ?>>
-								<a href="<?= $item[3] ?>">
-									<?= $item[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($item[0], \Catalyst\Page\Navigation\Navbar::NAVBAR) : $item[0] ?> 
+							<li<?= ($navbarItem[2] == PAGE_KEYWORD) ? ' class="active"' : "" ?>>
+								<a href="<?= $navbarItem[3] ?>">
+									<?= $navbarItem[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($navbarItem[0], \Catalyst\Page\Navigation\Navbar::NAVBAR) : $navbarItem[0] ?> 
 								</a>
 							</li>
 <?php break; ?>
 <?php case \Catalyst\Page\Navigation\Navbar::DROPDOWN_PARENT: ?>
-							<li<?= ($item[2] == PAGE_KEYWORD) ? ' class="active"' : "" ?>>
-								<a class="dropdown-button valign-wrapper" href="#!" data-activates="<?= $item[5] ?>">
-									<?= $item[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($item[0], \Catalyst\Page\Navigation\Navbar::NAVBAR) : $item[0] ?> 
+							<li<?= ($navbarItem[2] == PAGE_KEYWORD) ? ' class="active"' : "" ?>>
+								<a class="dropdown-button valign-wrapper" href="#!" data-activates="<?= $navbarItem[5] ?>">
+									<?= $navbarItem[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($navbarItem[0], \Catalyst\Page\Navigation\Navbar::NAVBAR) : $navbarItem[0] ?> 
 									<i class="material-icons right">arrow_drop_down</i>
 								</a>
 							</li>
-							<ul id="<?= $item[5] ?>" class="dropdown-content">
+							<ul id="<?= $navbarItem[5] ?>" class="dropdown-content">
 <?php break; ?>
 <?php case \Catalyst\Page\Navigation\Navbar::DROPDOWN_CHILD: ?>
 								<li>
-									<a href="<?= $item[3] ?>">
-										<?= $item[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($item[0], \Catalyst\Page\Navigation\Navbar::NAVBAR) : $item[0] ?> 
+									<a href="<?= $navbarItem[3] ?>">
+										<?= $navbarItem[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($navbarItem[0], \Catalyst\Page\Navigation\Navbar::NAVBAR) : $navbarItem[0] ?> 
 									</a>
 								</li>
 <?php break; ?>
@@ -52,22 +52,22 @@
 <?php if (\Catalyst\User\User::isLoggedIn()): ?>
 <?= $_SESSION["user"]->getSidenavHTML() ?>
 <?php endif; ?>
-<?php foreach (\Catalyst\Page\Navigation\Navbar::getNavbarItems(\Catalyst\User\User::getPermissionScope()) as $item): ?>
-<?php switch ($item[4]): ?>
+<?php foreach (\Catalyst\Page\Navigation\Navbar::getNavbarItems(\Catalyst\User\User::getPermissionScope()) as $navbarItem): ?>
+<?php switch ($navbarItem[4]): ?>
 <?php case \Catalyst\Page\Navigation\Navbar::NORMAL_LINK: ?>
-				<li<?= ($item[2] == PAGE_KEYWORD) ? ' class="active"' : "" ?>>
-					<a href="<?= $item[3] ?>">
-						<?= $item[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($item[0], \Catalyst\Page\Navigation\Navbar::SIDENAV) : $item[0] ?> 
+				<li<?= ($navbarItem[2] == PAGE_KEYWORD) ? ' class="active"' : "" ?>>
+					<a href="<?= $navbarItem[3] ?>">
+						<?= $navbarItem[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($navbarItem[0], \Catalyst\Page\Navigation\Navbar::SIDENAV) : $navbarItem[0] ?> 
 					</a>
 				</li>
 <?php break; ?>
 <?php case \Catalyst\Page\Navigation\Navbar::DROPDOWN_PARENT: ?>
 				<li class="divider"></li>
-				<li class="no-padding<?= ($item[2] == PAGE_KEYWORD) ? ' active' : "" ?>">
+				<li class="no-padding<?= ($navbarItem[2] == PAGE_KEYWORD) ? ' active' : "" ?>">
 					<ul class="collapsible collapsible-accordion">
 						<li>
 							<a class="collapsible-header valign-wrapper" href="#!">
-								<?= $item[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($item[0], \Catalyst\Page\Navigation\Navbar::SIDENAV) : $item[0] ?> 
+								<?= $navbarItem[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($navbarItem[0], \Catalyst\Page\Navigation\Navbar::SIDENAV) : $navbarItem[0] ?> 
 								<i class="material-icons right">arrow_drop_down</i>
 							</a>
 							<div class="collapsible-body">
@@ -75,8 +75,8 @@
 <?php break; ?>
 <?php case \Catalyst\Page\Navigation\Navbar::DROPDOWN_CHILD: ?>
 									<li>
-										<a href="<?= $item[3] ?>">
-											<?= $item[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($item[0], \Catalyst\Page\Navigation\Navbar::SIDENAV) : $item[0] ?> 
+										<a href="<?= $navbarItem[3] ?>">
+											<?= $navbarItem[1] == \Catalyst\Page\Navigation\Navbar::CALLABLE ? call_user_func($navbarItem[0], \Catalyst\Page\Navigation\Navbar::SIDENAV) : $navbarItem[0] ?> 
 										</a>
 									</li>
 <?php break; ?>
