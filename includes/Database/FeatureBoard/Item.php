09103e6414078295b9386c5bbd30670a2940c23c
Fauxil Fox <fauxil_fox@furmail.net>
2018-01-17 20:12:10 -0500

    Feature db from url

--- includes/Database/FeatureBoard/Item.php
+++ includes/Database/FeatureBoard/Item.php
@@ -11,6 +11,51 @@ class Item {
 		return $stmt->fetchAll();
 	}
 
+	public static function getFromUrl(string $url) : ?array {
+		$stmt = $GLOBALS["dbh"]->prepare("
+			SELECT
+				`".DB_TABLES["feature_board_items"]."`.`ID`,
+				`".DB_TABLES["feature_board_items"]."`.`NAME`,
+				`".DB_TABLES["feature_board_items"]."`.`AUTOGEN_URL`,
+				`".DB_TABLES["feature_board_items"]."`.`CREATED_TS`,
+				`".DB_TABLES["feature_board_items"]."`.`AUTHOR_ID`,
+				`".DB_TABLES["feature_board_items"]."`.`STATUS` AS `STATUS_KEY`,
+				`".DB_TABLES["feature_board_statuses"]."`.`NAME` AS `STATUS`,
+				`".DB_TABLES["feature_board_statuses"]."`.`DESCRIPTION` AS `STATUS_DESCRIPTION`,
+				`".DB_TABLES["feature_board_groups"]."`.`NAME` AS `GROUP`,
+				`".DB_TABLES["feature_board_items"]."`.`INTRODUCTION`,
+				`".DB_TABLES["feature_board_items"]."`.`PROPOSAL`,
+				`".DB_TABLES["feature_board_items"]."`.`ACKNOWLEDGEMENT`,
+				`".DB_TABLES["feature_board_items"]."`.`FUTURE_SCOPE`,
+				`".DB_TABLES["feature_board_items"]."`.`DEVELOPER_NOTE`,
+				`".DB_TABLES["feature_board_items"]."`.`ESTIMATED_MANHOURS`
+			FROM
+				`".DB_TABLES["feature_board_items"]."`
+			INNER JOIN
+				`".DB_TABLES["feature_board_statuses"]."`
+				ON
+					`".DB_TABLES["feature_board_statuses"]."`.`INTERNAL_NAME`
+					=
+					`".DB_TABLES["feature_board_items"]."`.`STATUS`
+			INNER JOIN
+				`".DB_TABLES["feature_board_groups"]."`
+				ON
+					`".DB_TABLES["feature_board_groups"]."`.`INTERNAL_NAME`
+					=
+					`".DB_TABLES["feature_board_items"]."`.`GROUP`
+			WHERE
+				`".DB_TABLES["feature_board_items"]."`.`AUTOGEN_URL` = :AUTOGEN_URL
+			");
+		$stmt->bindParam(":AUTOGEN_URL", $url);
+		$stmt->execute();
+
+		if ($stmt->rowCount() == 0) {
+			return null;
+		} else {
+			return $stmt->fetchAll()[0];
+		}
+	}
+
 	public static function getVotes(int $id) : array {
 		$stmt = $GLOBALS["dbh"]->prepare("SELECT `VOTE_TYPE` FROM `".DB_TABLES["feature_board_votes"]."` WHERE `FEATURE_ID` = :FEATURE_ID;");
 		$stmt->bindParam(":FEATURE_ID", $id);
