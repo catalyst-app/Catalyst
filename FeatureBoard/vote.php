23d1d365b45cfbfe78957070167314f44510f743
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-01-17 20:10:03 -0500

    vote form handler

---
+++ FeatureBoard/vote.php
@@ -0,0 +1,25 @@
+<?php
+
+define("ROOTDIR", "../");
+define("REAL_ROOTDIR", "../");
+
+require_once REAL_ROOTDIR."includes/init.php";
+use \Catalyst\Database\FeatureBoard\Groups;
+use \Catalyst\Database\FeatureBoard\Item;
+use \Catalyst\User\User;
+
+if (!isset($_POST["feature"]) || !preg_match('/^\d+$/', $_POST["feature"]) || User::isLoggedOut() || !in_array($_POST["vote"], ["YES","MAYBE","NO","IRRELEVANT"]) || Item::hasVoted((int)($_POST["feature"]), $_SESSION["user"]->getId())) {
+	Response::send401(1, "Invalid Request");
+}
+
+$result = Item::castVote(
+	$_POST["feature"],
+	$_POST["vote"],
+	$_SESSION["user"]->getId()
+);
+
+if (!$result) {
+	Response::send500(2, "Unknown error");
+}
+
+Response::send200("Vote submitted");
