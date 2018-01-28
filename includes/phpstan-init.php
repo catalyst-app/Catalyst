73b1d2445c96e15a51351d900d6a8a35b1070e85
Fauxil Fox <fauxil_fox@furmail.net>
2018-01-28 13:27:28 -0500

    Install phpstan

---
+++ includes/phpstan-init.php
@@ -0,0 +1,11 @@
+<?php
+
+define("ROOTDIR", "");
+define("REAL_ROOTDIR", "");
+
+require_once __DIR__."/init.php";
+define("PAGE_COLOR", \Catalyst\Page\Values::DEFAULT_COLOR);
+
+define("PAGE_KEYWORD", \Catalyst\Page\Values::ABOUT_US[0]);
+define("PAGE_TITLE", \Catalyst\Page\Values::createTitle(\Catalyst\Page\Values::ABOUT_US[1], []));
+
