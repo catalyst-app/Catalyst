36350e7e51298d907ba73d0aa026dfc8c2e73aa4
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-01-29 10:58:44 -0500

    remove live beta message

--- Outdated.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h1>Live beta server is offline</h1>
-<h3><a href="/">Use last committed version</a></h3>
-<p>This version may not contain recent changes or actions performed on the live version.</p>
-<p>Additionally, any changes made on this version will <strong>not</strong> be saved.</p>
-</body>
-</html>
