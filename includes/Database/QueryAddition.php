5e8e3d39c223691dd269d43d44170ae5e0ef3958
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-01-29 11:02:10 -0500

    define queryaddition interface

---
+++ includes/Database/QueryAddition.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Catalyst\Database;
+
+/**
+ * Basic interface which contains the functions required in order to make the query addition
+ */
+interface QueryAddition {
+	/**
+	 * Get the parameters to bind
+	 * 
+	 * @return array Parameters to bind to the Query
+	 */
+	public function getParamtersToBind() : array;
+	/**
+	 * Get the query string
+	 * 
+	 * @return string Query string, MUST include the WHERE/JOIN/etc
+	 */
+	public function __toString() : string;
+}
