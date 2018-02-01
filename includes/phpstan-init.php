2d24e3aed096e2a891b96a2ca5f7e64e14a55955
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-01 14:10:23 -0500

    Add Discord integration and fix a bit

--- includes/phpstan-init.php
+++ includes/phpstan-init.php
@@ -3,9 +3,10 @@
 define("ROOTDIR", "");
 define("REAL_ROOTDIR", "");
 
-require_once __DIR__."/controller.php";
-define("PAGE_COLOR", \Catalyst\Page\Values::DEFAULT_COLOR);
+require_once __DIR__."/Controller.php";
+restore_error_handler();
 
+define("PAGE_COLOR", \Catalyst\Page\Values::DEFAULT_COLOR);
 define("PAGE_KEYWORD", \Catalyst\Page\Values::ABOUT_US[0]);
 define("PAGE_TITLE", \Catalyst\Page\Values::createTitle(\Catalyst\Page\Values::ABOUT_US[1], []));
 
