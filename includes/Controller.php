ba592212e507742b0b1c0e822c14517ed74323df
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-02 20:16:30 -0500

    Separate global modifications (ini and such) out of the Controller

--- includes/Controller.php
+++ includes/Controller.php
@@ -284,33 +284,4 @@ class Controller {
 	}
 }
 
-spl_autoload_register("\\Catalyst\\Controller::loadClass");
-
-// Choose what error handling we want to do
-
-if (Controller::isDevelMode()) {
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	ini_set('scream.enabled', 1);
-} else {
-	error_reporting(0);
-	ini_set('display_errors', 0);
-	ini_set('display_startup_errors', 0);
-	ini_set('scream.enabled', 0);
-}
-
-set_error_handler("\\Catalyst\\Controller::handleError", E_ALL);
-
-// LEGACY
-require_once __DIR__."/Secrets.php";
-require_once __DIR__."/Database/Connector.inc.php";
-
-require_once __DIR__."/vendor/autoload.php";
-
-if (!session_id()) {
-	ini_set("session.cookie_lifetime", 24*60*60);
-	ini_set("session.gc_maxlifetime", 24*60*60);
-	ini_set("session.name", "catalyst");
-	session_start();
-}
+require_once __DIR__.DIRECTORY_SEPARATOR."initializer.php";
