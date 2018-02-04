a66a923a4b8d709b70f1ed096608b288297dbb64
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-03 23:33:10 -0500

    Implement TextField's getJsValidator

--- includes/Form/TextField.php
+++ includes/Form/TextField.php
@@ -113,7 +113,25 @@ class TextField extends Field {
 	 * 
 	 * @return string The JS to validate the field
 	 */
-	public function getJsValidator() : string { /* TODO */ }
+	public function getJsValidator() : string {
+		$str = '';
+		if ($this->isRequired()) {
+			$str .= 'if (';
+			$str .= '$('.json_encode("#".$this->getId()).').val().length === 0';
+			$str .= ') {';
+			$str .= 'markInputInvalid('.json_encode('#'.$this->getId()).', '.json_encode($this->getErrorMessage($this->getMissingErrorCode())).');';
+			$str .= Form::CANCEL_SUBMISSION_JS;
+			$str .= '}';
+		}
+		$str .= 'if (';
+		$str .= '!(new RegExp('.json_encode($this->getPattern()).').test($('.json_encode("#".$this->getId()).').val()))';
+		$str .= ') {';
+		$str .= 'markInputInvalid('.json_encode('#'.$this->getId()).', '.json_encode($this->getErrorMessage($this->getInvalidErrorCode())).');';
+		$str .= Form::CANCEL_SUBMISSION_JS;
+		$str .= '}';
+
+		return $str;
+	}
 
 	/**
 	 * Return JS code to store the field's value in $formDataName
