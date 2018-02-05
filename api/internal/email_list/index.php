cf1e99572cff383a00871e397407e7570f695a5c
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-05 17:17:38 -0500

    Re-arrange email list api endpoint's `use` statments, and now send an email on addition to the list

--- api/internal/email_list/index.php
+++ api/internal/email_list/index.php
@@ -4,11 +4,10 @@ define("ROOTDIR", "../../../");
 define("REAL_ROOTDIR", "../../../");
 
 require_once REAL_ROOTDIR."includes/Controller.php";
-use \Catalyst\API\{Endpoint, ErrorCodes, Response};
+use \Catalyst\API\{Endpoint, Response};
 use \Catalyst\Database\{Column, Tables, ReplaceQuery};
+use \Catalyst\Email;
 use \Catalyst\Form\FormRepository;
-use \Catalyst\HTTPCode;
-use \Catalyst\User\User;
 
 Endpoint::init(true, 0);
 
@@ -24,5 +23,20 @@ $query->addColumn(new Column("CONTEXT", Tables::EMAIL_LIST));
 $query->addValue("Web registration: ".$_POST["context"]);
 
 $query->execute();
+
+Email::sendEmail(
+	[[$_POST["email"], $_POST["context"]]],
+	"Catalyst - Email List Registration",
+	'<html><head><style>'.Email::getCss().'</style></head><body><div class="container"><div class="section"><h1 class="center header hide-on-small-only">Email List Registration</h1><h3 class="center header hide-on-med-and-up">Email List Registration</h3></div><div class="section"><p class="flow-text">Thank you for registering for Catalyst\'s email list!  We\'re glad to see you\'re with us, and can\'t wait to keep you updated via email.</p><p>If you did not sign up for this list, contact abuse@catalystapp.co</p></div></div></body></html>',
+	implode("\r\n", [
+		"Email List Registration",
+		"",
+		"Thank you for registering for Catalyst\'s email list!  We\'re glad to see you\'re with us, and can\'t wait to keep you updated via email.",
+		"",
+		"If you did not sign up for this list, contact abuse@catalystapp.co immediately."
+	]),
+	Email::NO_REPLY_EMAIL,
+	Email::NO_REPLY_PASSWORD
+);
 
 Response::sendSuccessResponse("Success");
