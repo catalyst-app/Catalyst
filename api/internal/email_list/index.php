fa87b26d267365c7c3f5d9ad6e9553deb04b9d28
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-05 10:23:04 -0500

    Use checkServerSide

--- api/internal/email_list/index.php
+++ api/internal/email_list/index.php
@@ -6,23 +6,13 @@ define("REAL_ROOTDIR", "../../../");
 require_once REAL_ROOTDIR."includes/Controller.php";
 use \Catalyst\API\{Endpoint, ErrorCodes, Response};
 use \Catalyst\Database\{Column, Tables, ReplaceQuery};
+use \Catalyst\Form\FormRepository;
 use \Catalyst\HTTPCode;
 use \Catalyst\User\User;
 
 Endpoint::init(true, 0);
 
-if (!isset($_POST["email"])) {
-	HTTPCode::set(400);
-	Response::sendErrorResponse(90001, ErrorCodes::ERR_90001);
-}
-if (!preg_match('/^.{2,}@.{2,}\..{2,}$/', $_POST["email"])) {
-	HTTPCode::set(400);
-	Response::sendErrorResponse(90002, ErrorCodes::ERR_90002);
-}
-if (!isset($_POST["context"])) {
-	HTTPCode::set(400);
-	Response::sendErrorResponse(90003, ErrorCodes::ERR_90003);
-}
+FormRepository::getEmailListAdditionForm()->checkServerSide();
 
 $query = new ReplaceQuery();
 $query->setTable(Tables::EMAIL_LIST);
