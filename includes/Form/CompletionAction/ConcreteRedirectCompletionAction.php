efc4faae22441a1b9a078756604eb8281e81cb95
Fauxil Fox <fauxil_fox@furmail.net>
2018-02-05 21:59:17 -0500

    Concrete redirect action

---
+++ includes/Form/CompletionAction/ConcreteRedirectCompletionAction.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Catalyst\Form\CompletionAction;
+
+/**
+ * Redirects to a given URL
+ */
+class ConcreteRedirectCompletionAction extends AbstractCompletionAction {
+	/**
+	 * URL to redirect to
+	 * 
+	 * @var string
+	 */
+	protected $redirectUrl = '';
+
+	/**
+	 * Create a new ConcreteRedirectCompletionAction
+	 * 
+	 * @param string $redirectUrl The URL to redirect to
+	 */
+	public function __construct(string $redirectUrl='Thank you for submitting this form.') {
+		$this->setRedirectUrl($redirectUrl);
+	}
+
+	/**
+	 * Get the redirect URL
+	 * 
+	 * @return string redirect URL
+	 */
+	public function getRedirectUrl() : string {
+		return $this->redirectUrl;
+	}
+
+	/**
+	 * Set the redirect URL
+	 * 
+	 * @param string $redirectUrl New redirect URL
+	 */
+	public function setRedirectUrl(string $redirectUrl) : void {
+		$this->redirectUrl = $redirectUrl;
+	}
+
+	/**
+	 * Get JavaScript to run on success
+	 * 
+	 * @return string JS code for success
+	 */
+	public function getJs() : string {
+		$str = '';
+		$str .= 'window.location = $("html").attr("data-rootdir")+'.json_encode($this->getRedirectUrl()).';';
+		return $str;
+	}
+}
+
