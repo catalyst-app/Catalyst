8e4719f281ed335e6399f3ebf7d5916f333ebdfa
Fauxil Fox <fauxil_fox@furmail.net>
2018-02-06 16:56:24 -0500

    Make logout page up-to-spec

--- Logout/index.php
+++ Logout/index.php
@@ -4,7 +4,6 @@ define("ROOTDIR", "../");
 define("REAL_ROOTDIR", "../");
 
 require_once REAL_ROOTDIR."includes/Controller.php";
-use \Catalyst\User\User;
 use \Catalyst\Page\{UniversalFunctions, Values};
 
 $_SESSION = [];
@@ -12,18 +11,15 @@ $_SESSION = [];
 define("PAGE_KEYWORD", Values::LOGOUT[0]);
 define("PAGE_TITLE", Values::createTitle(Values::LOGOUT[1], []));
 
-if (User::isLoggedIn()) {
-	define("PAGE_COLOR", User::getCurrentUser()->getColorHex());
-} else {
-	define("PAGE_COLOR", Values::DEFAULT_COLOR);
-}
+define("PAGE_COLOR", Values::DEFAULT_COLOR);
 
 require_once Values::HEAD_INC;
 
 echo UniversalFunctions::createHeading("Logout");
 ?>
 			<div class="section">
-				<p class="flow-text">You have been logged out.  <a href="<?= ROOTDIR ?>">Return home</p></a>
+				<p class="flow-text">You have been logged out.</p>
+				<p class="flow-text">Would you like to <a href="<?= ROOTDIR ?>">return home</p> or <a href="<?= ROOTDIR ?>Login">login again</a>?</a>
 			</div>
 <?php
 require_once Values::FOOTER_INC;
