608d35e989856c7113e6d692e0d295cb69239d5f
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-06 15:59:40 -0500

    add TOTP login form to repository

--- includes/Form/FormRepository.php
+++ includes/Form/FormRepository.php
@@ -4,7 +4,7 @@ namespace Catalyst\Form;
 
 use \Catalyst\API\ErrorCodes;
 use \Catalyst\Form\CompletionAction\{AutoClosingModalCompletionAction,ConcreteRedirectCompletionAction};
-use \Catalyst\Form\Field\{CaptchaField,EmailField,PasswordField,TextField};
+use \Catalyst\Form\Field\{CaptchaField,EmailField,PasswordField,StaticHTMLField,TextField};
 use \Catalyst\Form\Form;
 use \Catalyst\Secrets;
 use \ReflectionClass;
@@ -133,6 +133,48 @@ class FormRepository {
 		$captchaField->addError(90107, ErrorCodes::ERR_90107);
 		$captchaField->setInvalidErrorCode(90107);
 		$form->addField($captchaField);
+
+		return $form;
+	}
+
+	/**
+	 * TOTP Login form
+	 * 
+	 * See /Login/TOTP for form usage
+	 * 
+	 * @return Form Form for attempting a TOTP login
+	 */
+	public static function getTotpLoginForm() : Form {
+		$form = new Form();
+
+		$form->setDistinguisher(self::getDistinguisherFromFunctionName(__FUNCTION__)); // get-dash-case from camelCase
+		$form->setMethod(Form::POST);
+		$form->setEndpoint("internal/totp_login/");
+		$form->setButtonText("LOGIN");
+		$form->setPrimary(false);
+
+		$completionAction = new ConcreteRedirectCompletionAction();
+		$completionAction->setRedirectUrl("Dashboard");
+		$form->setCompletionAction($completionAction);
+
+		$usernameField = new TextField();
+		$usernameField->setDistinguisher("totp-code");
+		$usernameField->setLabel("Authentication Code");
+		$usernameField->setRequired(true);
+		$usernameField->setPattern('^[0-9]{6}$');
+		$usernameField->addError(90202, ErrorCodes::ERR_90202);
+		$usernameField->setMissingErrorCode(90202);
+		$usernameField->addError(90203, ErrorCodes::ERR_90203);
+		$usernameField->setInvalidErrorCode(90203);
+		$form->addField($usernameField);
+
+		$goBackNotice = new StaticHTMLField();
+		$goBackNotice->setHtml('<p class="col s12 no-margin">If you would like to go back and login with a different account, click <a href="'.ROOTDIR.'/Login/">here</a></p>');
+		$form->addField($goBackNotice);
+
+		$noCodeMessage = new StaticHTMLField();
+		$noCodeMessage->setHtml('<p class="col s12 no-top-margin">If you do not have access to this code, please <a href="'.ROOTDIR.'Help">contact support</a> with your recovery key.</p>');
+		$form->addField($noCodeMessage);
 
 		return $form;
 	}
