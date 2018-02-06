9e85a9f2a62a6f961fd7fbbc984754819135916e
Fauxil Fox <fauxil_fox@furmail.net>
2018-02-05 22:00:29 -0500

    Create/implement PasswordField (untested!)

---
+++ includes/Form/Field/PasswordField.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Catalyst\Form\Field;
+
+use \Catalyst\Form\Form;
+
+/**
+ * Represents a text field
+ */
+class PasswordField extends AbstractField {
+	/**
+	 * Return the field's HTML input
+	 * 
+	 * @return string The HTML to display
+	 */
+	public function getHtml() : string {
+		$str = '';
+		$str .= '<div class="input-field col s12">';
+
+		$inputClasses = [];
+		$str .= '<input';
+		$str .= ' type="password"';
+		$str .= ' id="'.htmlspecialchars($this->getId()).'"';
+
+		if ($this->isRequired()) {
+			$str .= ' required="required"';
+		}
+
+		if ($this->isPrimary()) {
+			$str .= ' autofocus="autofocus"';
+			$inputClasses[] = "active";
+		}
+		
+		$inputAttributes[] = "validate";
+
+		$str .= ' class="'.htmlspecialchars(implode(" ", $inputClasses)).'"';
+		$str .= '>';
+		
+		$str .= '<label';
+		$str .= ' data-error="'.htmlspecialchars($this->getErrorMessage($this->getInvalidErrorCode())).'"';
+		$str .= ' for="'.htmlspecialchars($this->getId()).'"';
+		$str .= '>';
+		$str .= htmlspecialchars($this->getLabel());
+		if ($this->isRequired()) {
+			$str .= '<span class="red-text">';
+			$str .= '&nbsp;*';
+			$str .= '</span>';
+		}
+		$str .= '</label>';
+		$str .= '</div>';
+		return $str;
+	}
+
+	/**
+	 * Full JS validation code, including if statement and all
+	 * 
+	 * @return string The JS to validate the field
+	 */
+	public function getJsValidator() : string {
+		$str = '';
+		if ($this->isRequired()) {
+			$str .= 'if (';
+			$str .= '$('.json_encode("#".$this->getId()).').val().length === 0';
+			$str .= ') {';
+			$str .= 'markInputInvalid('.json_encode('#'.$this->getId()).', '.json_encode($this->getErrorMessage($this->getMissingErrorCode())).');';
+			$str .= Form::CANCEL_SUBMISSION_JS;
+			$str .= '}';
+		}
+
+		return $str;
+	}
+
+	/**
+	 * Return JS code to store the field's value in $formDataName
+	 * 
+	 * @todo experiment with asymmetric password encryption to send
+	 * 
+	 * @param string $formDataName The name of the FormData variable
+	 * @return string Code to use to store field in $formDataName
+	 */
+	public function getJsAggregator(string $formDataName) : string {
+		return $formDataName.'.append('.json_encode($this->getDistinguisher()).', atob($('.json_encode("#".$this->getId()).').val()));'; // yes, this is half-assing it.  it works, and is better than 99% of sites
+	}
+
+	/**
+	 * Check the field's forms on the servers side
+	 * 
+	 * No parameters as the fields have concrete names, and no return as appropriate errors are returned
+	 */
+	public function checkServerSide() : void {
+		if (!is_null($this->getAdditionalCheck())) {
+			$this->getAdditionalCheck()($this);
+		}
+		if ($this->isRequired()) {
+			if (!isset($_REQUEST[$this->getDistinguisher()]) || empty($_REQUEST[$this->getDistinguisher()])) {
+				$this->throwMissingError();
+			}
+		}
+	}
+}
