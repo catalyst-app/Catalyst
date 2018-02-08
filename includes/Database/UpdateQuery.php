f5a23c2b546c27c89f7580cf34bce79e152768dd
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-08 00:36:52 -0500

    add + implement updatequery

---
+++ includes/Database/UpdateQuery.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Catalyst\Database;
+
+use \Catalyst\Database\Database;
+use \Catalyst\HTTPCode;
+use \Catalyst\API\{Endpoint, Response};
+
+/**
+ * Represents a MySQL UPDATE query
+ */
+class UpdateQuery extends AbstractQuery {
+	/**
+	 * Executes the query
+	 * 
+	 * Bind columns and whatnot. Also fills in ? for PDO binding
+	 * 
+	 * @return bool if the query was successful
+	 */
+	public function execute() : bool {
+		$this->verifyIntegrity();
+
+		$initialQuery = "UPDATE `".$this->table."` SET ";;
+		
+		$initialQuery .= implode(" = ?, ", $this->columns)." = ?";
+
+		// additional
+		if (is_array($this->additionalCapabilities)) {
+			foreach ($this->additionalCapabilities as $additionalCapability) {
+				$initialQuery .= " ";
+				// each additional capability should verify integrity in its getQueryString
+				$initialQuery .= $additionalCapability->getQueryString();
+			}
+		}
+		$initialQuery .= ";";
+
+		$stmt = Database::getDbh()->prepare($initialQuery);
+
+		$stmt->execute($this->getParamtersToBind());
+
+		$this->result = Database::getDbh()->lastInsertId();
+
+		return true;
+	}
+}
