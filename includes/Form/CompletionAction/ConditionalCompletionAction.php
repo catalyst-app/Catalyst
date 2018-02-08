675409ab6fa1937f55f07d62f97bba8be88abacb
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-08 12:59:07 -0500

    Add + implement a ConditionalCompletionAction
    Will perform other completion actions based on whether or not various conditions pass/fail

---
+++ includes/Form/CompletionAction/ConditionalCompletionAction.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Catalyst\Form\CompletionAction;
+
+/**
+ * Redirects to a given URL
+ */
+class ConditionalCompletionAction extends AbstractCompletionAction {
+	/**
+	 * Conditions and associated actions
+	 * 
+	 * Format: [condition, action]
+	 * 
+	 * @var mixed[][]
+	 */
+	protected $conditions = [];
+	/**
+	 * Action to run on else
+	 * 
+	 * @var AbstractCompletionAction|null
+	 */
+	protected $else = null;
+
+	/**
+	 * Create a new ConditionalCompletionAction
+	 * 
+	 * @param array $conditions Conditions
+	 */
+	public function __construct(array $conditions=[]) {
+		array_map(function ($condition) {
+			$this->addCondition(...$condition);
+		}, $conditions);
+	}
+
+	/**
+	 * Add a condition
+	 * 
+	 * @param string $condition Condition to test for the action
+	 * @param AbstractCompletionAction $action Action to perform if the condition is true
+	 */
+	public function addCondition(string $condition, AbstractCompletionAction $action) : void {
+		$this->conditions[] = [$condition, $action];
+	}
+
+	/**
+	 * Get the conditions
+	 * 
+	 * @return array The action's array of conditions
+	 */
+	public function getConditions() : array {
+		return $this->conditions;
+	}
+
+	/**
+	 * Set the conditions
+	 * 
+	 * @param array $conditions The new array to use
+	 */
+	public function setConditions(array $conditions) : void {
+		$this->conditions = [];
+		array_map(function ($condition) {
+			$this->addCondition(...$condition);
+		}, $conditions);
+	}
+
+	/**
+	 * Get the condition to run on else
+	 * 
+	 * @return AbstractCompletionAction Action to run if none of the conditions are true
+	 */
+	public function getElse() : ?AbstractCompletionAction {
+		return $this->else;
+	}
+
+	/**
+	 * Set the condition to run on else
+	 * 
+	 * @param AbstractCompletionAction $action Action to run if none of the conditions are true
+	 */
+	public function setElse(AbstractCompletionAction $action) : void {
+		$this->else = $action;
+	}
+
+	/**
+	 * Get JavaScript to run on success
+	 * 
+	 * @return string JS code for success
+	 */
+	public function getJs() : string {
+		if (count($this->getConditions()) + !is_null($this->getElse()) == 0) {
+			return '';
+		} else if (count($this->getConditions()) + !is_null($this->getElse()) == 1) {
+			if (!is_null($this->getElse())) {
+				return $this->getElse()->getJs();
+			} else {
+				return $this->getConditions()[0][1]->getJs();
+			}
+		}
+		$str = '';
+		for ($i=0; $i < count($this->getConditions()); $i++) { 
+			if ($i == 0) {
+				$str .= 'if';
+			} else {
+				$str .= 'else if';
+			}
+			$str .= '('.$this->getConditions()[$i][0].')';
+			$str .= '{';
+			$str .= $this->getConditions()[$i][1]->getJs();
+			$str .= '}';
+		}
+		if (!is_null($this->getElse())) {
+			$str .= 'else';
+			$str .= '{';
+			$str .= $this->getElse()->getJs();
+			$str .= '}';
+		}
+		$str .= 'window.location = $("html").attr("data-rootdir")+'.json_encode($this->getConditions()).';';
+		return $str;
+	}
+}
+
