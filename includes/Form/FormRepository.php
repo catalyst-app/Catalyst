5777b062de41c8d51703b6cb627fe74d9a840265
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-08 00:13:16 -0500

    define email verification form

--- includes/Form/FormRepository.php
+++ includes/Form/FormRepository.php
@@ -20,6 +20,7 @@ use \Catalyst\Form\Field\{
 	TextField};
 use \Catalyst\Form\Form;
 use \Catalyst\Secrets;
+use \Catalyst\Tokens;
 use \ReflectionClass;
 
 /**
@@ -351,6 +352,56 @@ class FormRepository {
 		$captchaField->setMissingErrorCode(90322);
 		$captchaField->addError(90323, ErrorCodes::ERR_90323);
 		$captchaField->setInvalidErrorCode(90323);
+		$form->addField($captchaField);
+
+		return $form;
+	}
+
+	/**
+	 * Verifies an email address token
+	 * 
+	 * See /EmailVerification
+	 * 
+	 * @return Form Form for verifying a new email
+	 */
+	public static function getEmailVerificationForm() : Form {
+		$form = new Form();
+
+		$form->setDistinguisher(self::getDistinguisherFromFunctionName(__FUNCTION__)); // get-dash-case from camelCase
+		$form->setMethod(Form::POST);
+		$form->setEndpoint("internal/email_verification/");
+		$form->setButtonText("VERIFY");
+		$form->setPrimary(true);
+
+		$completionAction = new ConcreteRedirectCompletionAction();
+		$completionAction->setRedirectUrl("Dashboard");
+		$form->setCompletionAction($completionAction);
+
+		$tokenField = new TextField();
+		$tokenField->setDistinguisher("token");
+		$tokenField->setLabel("Token");
+		$tokenField->setRequired(true);
+		if (array_key_exists("email_token", $_SESSION) && 
+			preg_match('/'.Tokens::EMAIL_VERIFICATION_TOKEN_REGEX.'/', $_SESSION["email_token"])) {
+			$tokenField->setPrefilledValue($_SESSION["email_token"]);
+		}
+		$tokenField->setMaxLength(Tokens::EMAIL_VERIFICATION_TOKEN_LENGTH);
+		$tokenField->setPattern(Tokens::EMAIL_VERIFICATION_TOKEN_REGEX);
+		$tokenField->addError(90401, ErrorCodes::ERR_90401);
+		$tokenField->setMissingErrorCode(90401);
+		$tokenField->addError(90402, ErrorCodes::ERR_90402);
+		$tokenField->setInvalidErrorCode(90402);
+		$form->addField($tokenField);
+
+		$captchaField = new CaptchaField();
+		$captchaField->setDistinguisher("captcha");
+		$captchaField->setRequired(true);
+		$captchaField->setSiteKey("6LdGBEEUAAAAAMHsFHz4BRvEnIq1NMuuU_Keo7nn");
+		$captchaField->setSecretKey(Secrets::EMAIL_VERIFICATION_CAPTCHA_SECRET);
+		$captchaField->addError(90403, ErrorCodes::ERR_90403);
+		$captchaField->setMissingErrorCode(90403);
+		$captchaField->addError(90404, ErrorCodes::ERR_90404);
+		$captchaField->setInvalidErrorCode(90404);
 		$form->addField($captchaField);
 
 		return $form;
