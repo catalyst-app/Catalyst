42507969985cbb62298403d0930a06282608d242
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-07 21:21:51 -0500

    Add sendVerificationEmail to User class

--- includes/User/User.php
+++ includes/User/User.php
@@ -407,6 +407,31 @@ class User implements \Serializable {
 		}
 	}
 
+	public function sendVerificationEmail() {
+		if ($this->emailIsVerified()) {
+			return;
+		}
+
+		preg_match("/^(.*)(EmailVerification|Register|Settings|api).*/", \Catalyst\Page\UniversalFunctions::getRequestURI(), $out);
+		$url = $out[1]."EmailVerification/?token=".$this->getEmailToken();
+
+		\Catalyst\Email::sendEmail(
+			[[$this->getEmail(), $this->getNickname()]],
+			"Catalyst - Email verification",
+			'<html><head><style>'.\Catalyst\Email::getCss($this->getColorHex()).'</style></head><body><div class="container"><div class="section"><h1 class="center header hide-on-small-only">Email Verification</h1><h3 class="center header hide-on-med-and-up">Email Verification</h3></div><div class="section"><p class="flow-text">Thank you for registering with Catalyst!</p><p class="flow-text">Please click the button below to activate your account.</p><div><a href="'.$url.'" class="btn">Verify</a></div><p>Alternatively, use the token <span style="font-weight: 700;">'.$this->getEmailToken().'</span> to verify your email.</p></div></div></body></html>',
+			implode("\r\n", [
+				"Email Verification",
+				"",
+				"Thank you for registering with Catalyst!",
+				"Please go to the following URL to verify your account:",
+				$url,
+				"Alternatively, use the token ".$this->getEmailToken()
+			]),
+			Email::NO_REPLY_EMAIL,
+			Email::NO_REPLY_PASSWORD
+		);
+	}
+
 	public function serialize() : string {
 		return $this->id;
 	}
