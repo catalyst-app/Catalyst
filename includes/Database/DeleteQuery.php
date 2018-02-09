59fb04e4aa98396ab8de825a969090774147e075
Fauxil Fox <fauxil_fox@furmail.net>
2018-02-09 13:27:35 -0500

    add new DeleteQuery

---
+++ includes/Database/DeleteQuery.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Catalyst\Database;
+
+use \Catalyst\Database\Database;
+
+/**
+ * Represents a MySQL DELETE query
+ */
+class DeleteQuery extends AbstractQuery {
+	/**
+	 * Executes the query
+	 * 
+	 * Bind columns and whatnot.  $values will NOT be used for this type of query
+	 * 
+	 * @return bool if the query was successful
+	 */
+	public function execute() : bool {
+		$this->verifyIntegrity();
+
+		$initialQuery = "DELETE";
+		
+		// from
+		$initialQuery .= " FROM `".$this->table."`";
+
+		// additional
+		if (is_array($this->additionalCapabilities)) {
+			foreach ($this->additionalCapabilities as $additionalCapability) {
+				$initialQuery .= " ";
+				// each additional capability should verify integrity in its getQueryString
+				$initialQuery .= $additionalCapability->getQueryString();
+			}
+		}
+		$initialQuery .= ";";
+
+		$stmt = Database::getDbh()->prepare($initialQuery);
+
+		$stmt->execute($this->getParamtersToBind());
+
+		$this->result = $stmt->fetchAll();
+
+		return true;
+	}
+}
