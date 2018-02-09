591d19520ea0d2fdb49c5839a5eda4ff55150b04
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-08 20:11:22 -0500

    add email check to settings endpoint

--- api/internal/settings/index.php
+++ api/internal/settings/index.php
@@ -33,3 +33,25 @@ if (count($query->getResult()) != 0) {
 	HTTPCode::set(400);
 	Response::sendErrorResponse(90503, ErrorCodes::ERR_90503);
 }
+
+// check email is free/own
+if (!empty($_POST["email"])) {
+	$query = new SelectQuery();
+	$query->setTable(Tables::USERS);
+	$query->addColumn(new Column("ID", Tables::USERS));
+	$whereClause = new WhereClause();
+	$whereClause->addToClause([new Column("EMAIL", Tables::USERS), "=", $_POST["email"]]);
+	$whereClause->addToClause(WhereClause::AND);
+	$whereClause->addToClause([new Column("ID", Tables::USERS), "!=", $id]);
+	$query->addAdditionalCapability($whereClause);
+	$query->execute();
+	if (count($query->getResult()) != 0) {
+		HTTPCode::set(400);
+		Response::sendErrorResponse(90503, ErrorCodes::ERR_90503);
+	}
+	if (strpos($_POST["email"], "@catalystapp.co") !== false) {
+		HTTPCode::set(400);
+		Response::sendErrorResponse(90523, ErrorCodes::ERR_90523);
+	}
+}
+
