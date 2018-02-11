bda9f686989d5bf2e386df9f10c4fa253e8006e9
Fauxil Fox <fauxil_fox@furmail.net>
2018-02-11 00:37:43 -0500

    add image folder constant class

---
+++ includes/Images/Folders.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Catalyst\Images;
+
+/**
+ * Contains constants related to image folders
+ */
+class Folders {
+	public const PROFILE_PHOTO => "profile_pictures",
+	public const CHARACTER_IMAGE => "character_images",
+	public const ARTIST_IMAGE => "artist_images",
+	public const COMMISSION_TYPE_IMAGE => "commission_type_images"
+}
