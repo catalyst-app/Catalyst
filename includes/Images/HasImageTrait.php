afb10079fab4cbbcfe1f76e0358110dd3fc596ce
Fauxil Fox <fauxil_fox@furmail.net>
2018-02-11 10:59:29 -0500

    add HasImageTrait for items with, well, images
    who'd've thunk that the has image trait means the thing has an image :thinking:

---
+++ includes/Images/HasImageTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Catalyst\Images;
+
+/**
+ * Used by all models with an image
+ */
+trait HasImageTrait {
+	/**
+	 * The object's image
+	 * @var null|Image
+	 */
+	protected $image=null;
+
+	abstract public function initializeImage() : void;
+
+	/**
+	 * Get the object's Image
+	 * 
+	 * @return Image
+	 */
+	public function getImage() : Image {
+		if (is_null($this->image)) {
+			$this->initializeImage();
+		}
+		return $this->image;
+	}
+
+	/**
+	 * Set the object's image
+	 * 
+	 * @param Image $image
+	 */
+	public function setImage(Image $image) : void {
+		$this->image = $image;
+	}
+}
