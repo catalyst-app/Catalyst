69d2f90e25bc5d001d91c8c55bb0394fd6105218
Fauxil Fox <fauxil_fox@furmail.net>
2018-02-12 16:46:40 -0500

    add integration icons definition to Folders

--- includes/Images/Folders.php
+++ includes/Images/Folders.php
@@ -10,5 +10,6 @@ class Folders {
 	public const CHARACTER_IMAGE = "character_images";
 	public const ARTIST_IMAGE = "artist_images";
 	public const COMMISSION_TYPE_IMAGE = "commission_type_images";
+	public const INTEGRATION_ICONS = "integration_icons";
 	public const GLOBAL_IMG = "img";
 }
