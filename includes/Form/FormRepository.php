edf11c9414efbdf86624ac36d884001e4c2336ff
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-12 16:46:23 -0500

    getAddNetworkLinkForm in FormRepository

--- includes/Form/FormRepository.php
+++ includes/Form/FormRepository.php
@@ -636,6 +636,57 @@ class FormRepository {
 	}
 
 	/**
+	 * Adding a social media network (link) form
+	 * 
+	 * See /Dashboard and other similar for fomr usage
+	 * 
+	 * @return Form Form for adding a social network via. a link
+	 */
+	public static function getAddNetworkLinkForm() : Form {
+		$form = new Form();
+
+		$form->setDistinguisher(self::getDistinguisherFromFunctionName(__FUNCTION__)); // get-dash-case from camelCase
+		$form->setMethod(Form::POST);
+		$form->setEndpoint("internal/social_media/add/");
+		$form->setButtonText("ADD");
+		$form->setPrimary(false);
+
+		// temporary
+		$completionAction = new ConcreteRedirectCompletionAction();
+		$completionAction->setRedirectUrl("Login");
+		$form->setCompletionAction($completionAction);
+
+		$labelField = new TextField();
+		$labelField->setDistinguisher("label");
+		$labelField->setLabel("Label");
+		$labelField->setRequired(true);
+		$labelField->setPattern('^.{2,64}$');
+		$labelField->addError(90702, ErrorCodes::ERR_90702);
+		$labelField->setMissingErrorCode(90702);
+		$labelField->addError(90703, ErrorCodes::ERR_90703);
+		$labelField->setInvalidErrorCode(90703);
+		$form->addField($labelField);
+
+		$urlField = new TextField();
+		$urlField->setDistinguisher("url");
+		$urlField->setLabel("URL or email");
+		$urlField->setRequired(true);
+		$urlField->setPattern('^(https?://.{2,}|.{2,}@.{2,}\..{2,})$');
+		$urlField->addError(90704, ErrorCodes::ERR_90704);
+		$urlField->setMissingErrorCode(90704);
+		$urlField->addError(90705, ErrorCodes::ERR_90705);
+		$urlField->setInvalidErrorCode(90705);
+		$urlField->addError(90706, ErrorCodes::ERR_90706);
+		$urlField->addError(90707, ErrorCodes::ERR_90707);
+		$urlField->addError(90708, ErrorCodes::ERR_90708);
+		$urlField->addError(90709, ErrorCodes::ERR_90709);
+		$urlField->addError(90710, ErrorCodes::ERR_90710);
+		$form->addField($urlField);
+
+		return $form;
+	}
+
+	/**
 	 * Get all Forms functions defined in the repository
 	 * 
 	 * @return Form[] All forms in the repository
