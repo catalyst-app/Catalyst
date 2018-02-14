ce8c4ef5879818eb38b29866d4725a671c48666a
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-14 12:43:22 -0500

    Revert "Improved script inclusion"
    This reverts commit f342c9c01b745fd0b6a41bf254dcf1bb01239674.

--- includes/Page/Header/Header.php
+++ includes/Page/Header/Header.php
@@ -4,13 +4,13 @@ namespace Catalyst\Page\Header;
 
 class Header {
 	const SCRIPTS = [
-		["https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js", "async", "defer", 'crossorigin="anonymous"'],
-		["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.js", 'crossorigin="anonymous"'],
-		["https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.3/draggable.bundle.min.js", "async", 'crossorigin="anonymous"'],
-		["https://cdn.rawgit.com/namuol/cheet.js/master/cheet.min.js", "async", "defer", 'crossorigin="anonymous"'],
+		["https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"],
+		["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.js"],
+		["https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.3/draggable.bundle.min.js"],
+		["https://cdn.rawgit.com/namuol/cheet.js/master/cheet.min.js", "defer"],
 		["https://www.google.com/recaptcha/api.js", "defer"],
-		[ROOTDIR."js/markdown-it.js", "async", "defer"],
-		[ROOTDIR."js/jq-ajax-progress.js", "async", "defer"],
+		[ROOTDIR."js/markdown-it.js", "defer"],
+		[ROOTDIR."js/jq-ajax-progress.js", "defer"],
 		[ROOTDIR."js/main.js.php"],
 	];
 
