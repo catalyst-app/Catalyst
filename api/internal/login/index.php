7ee256005f411288583b704e99610f8cfe0e7bfd
Fauxil Fox <fauxil_fox@furmail.net>
2018-02-22 15:48:12 -0500

    Add logic to login handler to rehash password if needed
    Allows modular-er chaning of costs

--- api/internal/login/index.php
+++ api/internal/login/index.php
@@ -5,9 +5,10 @@ define("REAL_ROOTDIR", "../../../");
 
 require_once REAL_ROOTDIR."includes/Controller.php";
 use \Catalyst\API\{Endpoint, ErrorCodes, Response};
-use \Catalyst\Database\{Column, Tables, SelectQuery, WhereClause};
+use \Catalyst\Database\{Column, Tables, SelectQuery, UpdateQuery, WhereClause};
 use \Catalyst\{Email, HTTPCode};
 use \Catalyst\Form\FormRepository;
+use \Catalyst\Page\Values;
 use \Catalyst\User\User;
 
 Endpoint::init(true, 2);
@@ -49,6 +50,21 @@ if ($result[0]["SUSPENDED"]) {
 if ($result[0]["DEACTIVATED"]) {
 	HTTPCode::set(400);
 	Response::sendErrorResponse(90109, ErrorCodes::ERR_90109);
+}
+
+if (password_needs_rehash($result[0]["HASHED_PASSWORD"], PASSWORD_BCRYPT, ["cost" => Values::BCRYPT_COST])) {
+	$stmt = new UpdateQuery();
+
+	$stmt->setTable(Tables::USERS);
+
+	$stmt->addColumn(new Column("HASHED_PASSWORD", Tables::USERS));
+	$stmt->addValue(password_hash($_POST["password"], PASSWORD_BCRYPT, ["cost" => Values::BCRYPT_COST]));
+
+	$whereClause = new WhereClause();
+	$whereClause->addToClause([new Column("ID", Tables::USERS), '=', $result[0]["ID"]]);
+	$stmt->addAdditionalCapability($whereClause);
+
+	$stmt->execute();
 }
 
 if ($result[0]["TOTP_KEY"] !== null) {
