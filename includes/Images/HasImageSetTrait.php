d9cedc36811772fc64ebba1aab5581ca2dadd07b
Fauxil Fox <fauxil_fox@furmail.net>
2018-02-22 16:31:56 -0500

    add a HasImageSetTrait for objects which have a set of images
    e.g. characters, commissiontypes, etc

---
+++ includes/Images/HasImageSetTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Catalyst\Images;
+
+/**
+ * Used by all models with several images (examples, etc)
+ */
+trait HasImageSetTrait {
+	/**
+	 * The object's images
+	 * @var Image[]|null
+	 */
+	protected $images=null;
+
+	abstract public function initializeImageSet() : void;
+
+	/**
+	 * Get the object's Image
+	 * 
+	 * @return Image[]
+	 */
+	public function getImageSet() : array {
+		if (is_null($this->images)) {
+			$this->initializeImage();
+		}
+		return $this->images;
+	}
+
+	/**
+	 * Set the object's images
+	 * 
+	 * @param Image[] $images
+	 */
+	public function setImageSet(array $images) : void {
+		$this->images = $images;
+	}
+}
