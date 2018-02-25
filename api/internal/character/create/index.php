61aba32a95e8962a99634b27c8d2950b1b2d614f
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-02-25 12:29:23 -0500

    use the aforementioned sort field in character creation

--- api/internal/character/create/index.php
+++ api/internal/character/create/index.php
@@ -54,6 +54,7 @@ if (isset($_FILES["images"])) {
 		$stmt->addColumn(new Column("PATH", Tables::CHARACTER_IMAGES));
 		$stmt->addColumn(new Column("NSFW", Tables::CHARACTER_IMAGES));
 		$stmt->addColumn(new Column("PRIMARY", Tables::CHARACTER_IMAGES));
+		$stmt->addColumn(new Column("SORT", Tables::CHARACTER_IMAGES));
 
 		$primaryHasBeenAdded = false;
 
@@ -64,6 +65,7 @@ if (isset($_FILES["images"])) {
 			$stmt->addValue($image->getPath());
 			$stmt->addValue($imageMeta[$image->getUploadName()]["nsfw"] ? 1 : 0);
 			$stmt->addValue($primaryHasBeenAdded ? 0 : 1); // if already set, then 0
+			$stmt->addValue($imageMeta[$image->getUploadName()]["sort"]);
 			$primaryHasBeenAdded = true;
 		}
 
