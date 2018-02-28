52e52e088cde638dbfad7acd829b445c47c81437
Fauxil Fox <fauxil_fox@furmail.net>
2018-02-28 16:35:48 -0500

    defer analytics scripts

--- includes/Page/Footer/footer.inc.php
+++ includes/Page/Footer/footer.inc.php
@@ -20,7 +20,7 @@ use \Catalyst\Page\UniversalFunctions;
 		</footer>
 	</body>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112460506-1"></script>
+	<script async defer src="https://www.googletagmanager.com/gtag/js?id=UA-112460506-1"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
 		function gtag(){dataLayer.push(arguments);}
