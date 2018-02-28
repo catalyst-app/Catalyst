93eb0b049591744681e867a6062b6e2e0d13b5b0
Fauxil Fox <fauxil_fox@furmail.net>
2018-02-27 22:15:24 -0500

    shhh secrets

--- js/main.js.php
+++ js/main.js.php
@@ -1095,6 +1095,9 @@ function totp(K,t) {
 	});
 })(jQuery);
 
+// dont tell toish but i actually kinda like JS
+// its neat and fairly powerful, plus the community seems really neat
+// but dont you dare try to run it on the server
 console.log("'''''''''''''''''''''''''''''''';''''';'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''");
 console.log("'''''''''''''''''''''''''''',,',:0Od;':,''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''");
 console.log("''''''''''''''''''''''''',0MMMMMMMMMMW0:''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''");
