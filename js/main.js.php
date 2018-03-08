0317d7337f7fa0868038c543e3e345314009b5ba
Fauxil Fox <smileytechguy@smileytechguy.com>
2018-03-08 12:47:19 -0500

    wrap JS error handler in a try/catch so the error handler won't throw errors

--- js/main.js.php
+++ js/main.js.php
@@ -107,20 +107,22 @@ if (!Array.prototype.includes) {
 }
 
 window.onerror = function(message, url, lineNumber) {  
-	var data = new FormData();
-	data.append("message", message);
-	data.append("url", url);
-	data.append("lineNumber", lineNumber);
-	$.ajax($("html").attr("data-rootdir")+"api/internal/js_error/", {
-		data: data,
-		processData: false,
-		contentType: false,
-		method: "POST"
-	}).done(function(response) {
-		Materialize.toast("An error occured", 4000);
-	}).fail(function(response) {
-		Materialize.toast("An error occured", 4000);
-	});
+	try {
+		var data = new FormData();
+		data.append("message", message);
+		data.append("url", url);
+		data.append("lineNumber", lineNumber);
+		$.ajax($("html").attr("data-rootdir")+"api/internal/js_error/", {
+			data: data,
+			processData: false,
+			contentType: false,
+			method: "POST"
+		}).done(function(response) {
+			Materialize.toast("An error occured", 4000);
+		}).fail(function(response) {
+			Materialize.toast("An error occured", 4000);
+		});
+	} catch (e) {}
 	return false;
 };
 
