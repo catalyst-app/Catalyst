e6a4eee34e3ec5039f3e01068e1da95909cc25c5
Fauxil Fox <fauxil_fox@furmail.net>
2018-03-08 01:14:45 -0500

    adjust whitespace for consistency

--- Help/ToSGuide/index.php
+++ Help/ToSGuide/index.php
@@ -246,8 +246,11 @@ echo UniversalFunctions::createHeading("Terms of Service Guide");
 				<div class="divider"></div>
 				<div class="section" id="sharing-of-finished-work">
 					<h4>Sharing Of Finished Work</h4>
+
 					<p class="flow-text">Protecting a creator's intellectual property is important both in terms of recognition and to prevent improper use of the artwork by the commissioner. While making a ToS, creators should think of a few key aspects: can their artwork be modified, where it can be shown, the conditions for being able to showcase the art, can a commissioner use the artwork for profit and the rights that the creator keeps over the artwork (the idea, setting, recreating the work, right to profit from the artwork). 
+					
 					<p class="flow-text">We recommend that creators and commissioners establish thorough boundaries ahead of time so that both the commissioner and artist knows what can or cannot done with the artwork and to prevent creating any disputes in the future. </p>
+					
 					<p class="flow-text no-bottom-margin">Examples we like:</p>
 					<ul class="browser-default">
 						<li class="flow-text no-margin">
@@ -264,12 +267,12 @@ echo UniversalFunctions::createHeading("Terms of Service Guide");
 							<p class="no-margin">You may upload the piece to any website so long as you give credit to me in the description.  I do take private commissions (commissions I will not post or use for prints), but require a privacy fee of 40% to cover advertisement and possible revenue losses.  Under no circumstances will I remove art from my gallery if the privacy fee hasn’t been paid.	This does not give the user commercial rights, simply the right to keep the commission private or released only at their discretion. This does not apply to timed privacy up to a month.  In the instance of a gift that you’d rather not have me post before a certain time, I do not charge the additional fee, as long as it’s within one calendar month from the time of completion.</p>
 							<p class="right-align no-margin"><em>Adapted from <a href="http://stealthnachos.weebly.com/terms-of-service.html">Stealthnachos</a>' ToS</em></p>
 						</li>
+
 						<li class="flow-text no-margin">
 							<p class="no-margin">Once completed I will link the files to you via dropbox unless you have provided me with a designated email address for the files to be sent to. </p>
 							<p class="right-align no-margin"><em>Adapted from <a href="https://docs.google.com/document/d/1d5yqWdobmAwc4rJh6cfvABh-CarNI6zzxh-QXP8fSr4/edit">Bunnybb</a>'s TOS</em></p>
 						</li>
 					</ul>
-
 				</div>
 				<div class="divider"></div>
 				<div class="section" id="copyright">
