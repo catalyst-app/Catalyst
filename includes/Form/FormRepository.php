f49834ac6b5c347d10eb5825beba5f9ca46a86e5
Fauxil Fox <fauxil_fox@furmail.net>
2018-03-09 11:20:42 -0500

    add artist deletion form to repo

--- includes/Form/FormRepository.php
+++ includes/Form/FormRepository.php
@@ -1132,6 +1132,33 @@ class FormRepository {
 	}
 
 	/**
+	 * Get the form used to delete an artist page
+	 * 
+	 * @return Form
+	 */
+	public static function getDeleteArtistPageForm() : Form {
+		$form = new Form();
+
+		$form->setDistinguisher(self::getDistinguisherFromFunctionName(__FUNCTION__)); // get-dash-case from camelCase
+		$form->setMethod(Form::POST);
+		$form->setEndpoint("internal/artist/delete/");
+		$form->setButtonText("DELETE");
+		$form->setPrimary(false);
+
+		$completionAction = new ConcreteRedirectCompletionAction();
+		$completionAction->setRedirectUrl("Dashboard");
+		$form->setCompletionAction($completionAction);
+
+		$confirmField = new JSConfirmField();
+		$confirmField->setDistinguisher("confirm");
+		$confirmField->setRequired(true);
+		$confirmField->setPrompt("Are you sure you want to delete your page?  This will PERMANENTLY delete all commission types and other information.");
+		$form->addField($confirmField);
+
+		return $form;
+	}
+
+	/**
 	 * Get all Forms functions defined in the repository
 	 * @return Form[] All forms in the repository
 	 */
