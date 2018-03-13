c86434974bf4905783a05e6ab2c005f5a52ec8a7
Fauxil Fox <fauxil_fox@furmail.net>
2018-03-13 11:08:48 -0400

    add basic edit artist page form

--- includes/Form/FormRepository.php
+++ includes/Form/FormRepository.php
@@ -1159,6 +1159,155 @@ class FormRepository {
 	}
 
 	/**
+	 * Get the form used to edit an artist page
+	 * 
+	 * @return Form
+	 */
+	public static function getEditArtistPageForm() : Form {
+		$form = new Form();
+
+		$form->setDistinguisher(self::getDistinguisherFromFunctionName(__FUNCTION__)); // get-dash-case from camelCase
+		$form->setMethod(Form::POST);
+		$form->setEndpoint("internal/artist/edit/");
+		$form->setButtonText("SAVE");
+		$form->setPrimary(true);
+
+		$completionAction = new ConcreteRedirectCompletionAction();
+		$completionAction->setRedirectUrl("Artist");
+		$form->setCompletionAction($completionAction);
+
+		$nameField = new TextField();
+		$nameField->setDistinguisher("name");
+		$nameField->setLabel("Name");
+		$nameField->setRequired(true);
+		$nameField->setMaxLength(255);
+		$nameField->setPattern('^.{2,255}$');
+		$nameField->addError(91401, ErrorCodes::ERR_91401);
+		$nameField->setMissingErrorCode(91401);
+		$nameField->addError(91402, ErrorCodes::ERR_91402);
+		$nameField->setInvalidErrorCode(91402);
+		$form->addField($nameField);
+
+		$urlField = new TextField();
+		$urlField->setDistinguisher("url");
+		$urlField->setLabel("URL");
+		$urlField->setRequired(true);
+		$urlField->setMaxLength(255);
+		$urlField->setPattern('^[A-Za-z0-9._-]{3,254}[A-Za-z0-9_-]$');
+		$urlField->addError(91403, ErrorCodes::ERR_91403);
+		$urlField->setMissingErrorCode(91403);
+		$urlField->addError(91404, ErrorCodes::ERR_91404);
+		$urlField->setInvalidErrorCode(91404);
+		$urlField->addError(91405, ErrorCodes::ERR_91405);
+		$form->addField($urlField);
+
+		$urlSample = new StaticHTMLField();
+		
+		$urlSampleHtml = '';
+
+		$urlSampleHtml .= '<p';
+		$urlSampleHtml .= ' class="col s12 no-top-margin"';
+		$urlSampleHtml .= '>';
+
+		$urlSampleHtml .= 'This will be the link to your page: ';
+		
+		$urlSampleHtml .= '<strong';
+		$urlSampleHtml .= ' id="create-artist-page-url-sample"';
+		$urlSampleHtml .= ' data-base="'.htmlspecialchars((preg_replace('/New\/?$/', '', UniversalFunctions::getRequestUrl()))).'"';
+		$urlSampleHtml .= '>';
+		
+		$urlSampleHtml .= htmlspecialchars((preg_replace('/New\/?$/', '', UniversalFunctions::getRequestUrl())));
+		
+		$urlSampleHtml .= '</strong>';
+		
+		$urlSampleHtml .= '</p>';
+		
+		$urlSample->setHtml($urlSampleHtml);
+		
+		$form->addField($urlSample);
+
+		$descriptionField = new MarkdownField();
+		$descriptionField->setDistinguisher("description");
+		$descriptionField->setLabel("Description");
+		$descriptionField->setRequired(true);
+		$descriptionField->addError(91406, ErrorCodes::ERR_91406);
+		$descriptionField->setMissingErrorCode(91406);
+		$descriptionField->addError(91407, ErrorCodes::ERR_91407);
+		$descriptionField->setInvalidErrorCode(91407);
+		$form->addField($descriptionField);
+
+		$profilePictureField = new ImageField();
+		$profilePictureField->setDistinguisher("image");
+		$profilePictureField->setLabel("Image");
+		$profilePictureField->setRequired(false);
+		$profilePictureField->setMaxHumanSize('10MB');
+		// these lost some clarity as what means what due to ImageField
+		$profilePictureField->addError(91408, ErrorCodes::ERR_91408);
+		$profilePictureField->setMissingErrorCode(91408);
+		$profilePictureField->addError(91409, ErrorCodes::ERR_91409);
+		$profilePictureField->setInvalidErrorCode(91409);
+		$profilePictureField->addError(91410, ErrorCodes::ERR_91410);
+		$profilePictureField->setTooLargeErrorCode(91410);
+		$form->addField($profilePictureField);
+
+		$noNsfwWarning = new StaticHTMLField();
+		$noNsfwWarning->setHtml('<p class="col s12 no-top-margin">Artist\'s profile images may <strong>not</strong> be mature or explicit.</p>');
+		$form->addField($noNsfwWarning);
+
+		$colorField = new ColorField();
+		$colorField->setDistinguisher("color");
+		$colorField->setLabel("Color");
+		$colorField->setRequired(true);
+		$colorField->addError(91411, ErrorCodes::ERR_91411);
+		$colorField->setMissingErrorCode(91411);
+		$colorField->addError(91412, ErrorCodes::ERR_91412);
+		$colorField->setInvalidErrorCode(91412);
+		if (User::isLoggedIn()) {
+			$colorField->setPrefilledValue($_SESSION["user"]->getColor());
+		}
+		$form->addField($colorField);
+
+		$tosField = new MarkdownField();
+		$tosField->setDistinguisher("tos");
+		$tosField->setLabel("Terms of Service");
+		$tosField->setRequired(true);
+		$tosField->addError(91413, ErrorCodes::ERR_91413);
+		$tosField->setMissingErrorCode(91413);
+		$tosField->addError(91414, ErrorCodes::ERR_91414);
+		$tosField->setInvalidErrorCode(91414);
+		$form->addField($tosField);
+
+		$tosNote = new StaticHTMLField();
+		
+		$str = '';
+
+		$str .= '<p';
+		$str .= ' class="col s12 no-margin"';
+		$str .= '>';
+		$str .= 'We ';
+		$str .= '<strong>highly</strong>';
+		$str .= ' suggest that any creator, no matter what, have a ToS.';
+		$str .= '</p>';
+
+		$str .= '<p';
+		$str .= ' class="col s12 no-margin"';
+		$str .= '>';
+		$str .= 'We recommend including at least the following items: what you will not do, payment information, refund information, shipping information, sharing of finished art, commercial use, and changes to the finished product.';
+		$str .= '</p>';
+
+		$str .= '<p';
+		$str .= ' class="col s12 no-top-margin"';
+		$str .= '>';
+		$str .= 'Want ideas for what to include?  Check out our <a href="'.ROOTDIR.'Help/ToSGuide/">Terms of Service Guide</a>!';
+		$str .= '</p>';
+
+		$tosNote->setHtml($str);
+		$form->addField($tosNote);
+
+		return $form;
+	}
+
+	/**
 	 * Get all Forms functions defined in the repository
 	 * @return Form[] All forms in the repository
 	 */
