ec0bf2f1a5ccd9abbb98c96bc885836599f00497
Fauxil Fox <fauxil_fox@furmail.net>
2018-03-22 14:45:24 -0400

    move a few character forms out of repository and into individual

---
+++ includes/Character/NewCharacterFormTrait.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Catalyst\Character;
+
+use \Catalyst\API\ErrorCodes;
+use \Catalyst\Form\CompletionAction\DynamicRedirectCompletionAction;
+use \Catalyst\Form\Field\{CheckboxField,ColorField,HiddenInputField,MarkdownField,MultipleImageWithNsfwCaptionAndInfoField,StaticHTMLField,TextField};
+use \Catalyst\Form\Form;
+use \Catalyst\User\User;
+use \Catalyst\Secrets;
+
+/**
+ * new character form
+ */
+trait NewCharacterFormTrait {
+	/**
+	 * Get the form used to add a new character
+	 * 
+	 * See /Character/New
+	 * @return Form
+	 */
+	public static function getNewCharacterForm() : Form {
+		$form = new Form();
+
+		$form->setDistinguisher(self::getDistinguisherFromFunctionName(__FUNCTION__)); // get-dash-case from camelCase
+		$form->setMethod(Form::POST);
+		$form->setEndpoint("internal/character/create/");
+		$form->setButtonText("CREATE");
+		$form->setPrimary(true);
+
+		$completionAction = new DynamicRedirectCompletionAction();
+		$form->setCompletionAction($completionAction);
+
+		$nameField = new TextField();
+		$nameField->setDistinguisher("name");
+		$nameField->setLabel("Name");
+		$nameField->setRequired(true);
+		$nameField->setPattern('^.{2,255}$');
+		$nameField->setMaxLength(255);
+		$nameField->addError(90801, ErrorCodes::ERR_90801);
+		$nameField->setMissingErrorCode(90801);
+		$nameField->addError(90802, ErrorCodes::ERR_90802);
+		$nameField->setInvalidErrorCode(90802);
+		$form->addField($nameField);
+
+		$descriptionField = new MarkdownField();
+		$descriptionField->setDistinguisher("description");
+		$descriptionField->setLabel("Description");
+		$descriptionField->setRequired(true);
+		$descriptionField->addError(90803, ErrorCodes::ERR_90803);
+		$descriptionField->setMissingErrorCode(90803);
+		$descriptionField->addError(90804, ErrorCodes::ERR_90804);
+		$descriptionField->setInvalidErrorCode(90804);
+		$form->addField($descriptionField);
+
+		$imagesField = new MultipleImageWithNsfwCaptionAndInfoField();
+		$imagesField->setDistinguisher("images");
+		$imagesField->setLabel("Images");
+		$imagesField->setRequired(false);
+		$imagesField->setMaxHumanSize('10MB');
+		$imagesField->setInfoLabel('Artist/Source');
+		$imagesField->addError(90806, ErrorCodes::ERR_90806);
+		$imagesField->setMissingErrorCode(90806);
+		$imagesField->addError(90807, ErrorCodes::ERR_90807);
+		$imagesField->setInvalidErrorCode(90807);
+		$imagesField->addError(90805, ErrorCodes::ERR_90805);
+		$imagesField->setTooLargeErrorCode(90805);
+		$form->addField($imagesField);
+
+		$colorField = new ColorField();
+		$colorField->setDistinguisher("color");
+		$colorField->setLabel("Color");
+		$colorField->setRequired(true);
+		if (User::isLoggedIn()) {
+			$colorField->setPrefilledValue($_SESSION["user"]->getColor());
+		}
+		$colorField->addError(90808, ErrorCodes::ERR_90808);
+		$colorField->setMissingErrorCode(90808);
+		$colorField->addError(90809, ErrorCodes::ERR_90809);
+		$colorField->setInvalidErrorCode(90809);
+		$form->addField($colorField);
+
+		$publicCheckboxField = new CheckboxField();
+		$publicCheckboxField->setDistinguisher("public");
+		$publicCheckboxField->setLabel("Make this character public");
+		$publicCheckboxField->setRequired(false);
+		$publicCheckboxField->addError(90810, ErrorCodes::ERR_90810);
+		$publicCheckboxField->setMissingErrorCode(90810);
+		$publicCheckboxField->addError(90811, ErrorCodes::ERR_90811);
+		$publicCheckboxField->setInvalidErrorCode(90811);
+		$form->addField($publicCheckboxField);
+
+		$publicNotice = new StaticHTMLField();
+		$publicNotice->setHtml('<p class="col s12 no-margin">If this character is public, anyone can see it on your profile and access it with its link.  Otherwise, only you and artists you commission may see it.</p>');
+		$form->addField($publicNotice);
+
+		return $form;
+	}
+}
