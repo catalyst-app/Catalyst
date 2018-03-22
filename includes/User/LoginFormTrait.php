69ff1b72e113d9aefd3064f97563e9c1648a1320
Fauxil Fox <fauxil_fox@furmail.net>
2018-03-22 09:51:08 -0400

    actually commit files relevant to the commit

---
+++ includes/User/LoginFormTrait.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Catalyst\User;
+
+use \Catalyst\API\ErrorCodes;
+use \Catalyst\Form\CompletionAction\ConcreteRedirectCompletionAction;
+use \Catalyst\Form\Field\{TextField,PasswordField,CaptchaField};
+use \Catalyst\Form\Form;
+use \Catalyst\Secrets;
+
+/**
+ * Login form
+ */
+trait LoginFormTrait {
+	/**
+	 * Login form
+	 * 
+	 * See /Login for form usage
+	 * @return Form Form for attempting a login
+	 */
+	public static function getLoginForm() : Form {
+		$form = new Form();
+
+		$form->setDistinguisher(self::getDistinguisherFromFunctionName(__FUNCTION__)); // get-dash-case from camelCase
+		$form->setMethod(Form::POST);
+		$form->setEndpoint("internal/login/");
+		$form->setButtonText("LOGIN");
+		$form->setPrimary(true);
+
+		$completionAction = new ConcreteRedirectCompletionAction();
+		$completionAction->setRedirectUrl("Dashboard");
+		$form->setCompletionAction($completionAction);
+
+		$totpAction = new ConcreteRedirectCompletionAction();
+		$totpAction->setRedirectUrl("Login/TOTP");
+		$form->addAdditionalCases(90110, $totpAction);
+
+		$usernameField = new TextField();
+		$usernameField->setDistinguisher("username");
+		$usernameField->setLabel("Username");
+		$usernameField->setRequired(true);
+		$usernameField->setPattern('^([A-Za-z0-9._-]){2,64}$');
+		$usernameField->addError(90101, ErrorCodes::ERR_90101);
+		$usernameField->setMissingErrorCode(90101);
+		$usernameField->addError(90102, ErrorCodes::ERR_90102);
+		$usernameField->setInvalidErrorCode(90102);
+		$usernameField->addError(90103, ErrorCodes::ERR_90103);
+		$usernameField->addError(90108, ErrorCodes::ERR_90108);
+		$usernameField->addError(90109, ErrorCodes::ERR_90109);
+		$form->addField($usernameField);
+
+		$passwordField = new PasswordField();
+		$passwordField->setDistinguisher("password");
+		$passwordField->setLabel("Password");
+		$passwordField->setRequired(true);
+		$passwordField->setMinLength(8);
+		$passwordField->addError(90104, ErrorCodes::ERR_90104);
+		$passwordField->setMissingErrorCode(90104);
+		$passwordField->addError(90105, ErrorCodes::ERR_90105);
+		$passwordField->setInvalidErrorCode(90105);
+		$form->addField($passwordField);
+
+		$captchaField = new CaptchaField();
+		$captchaField->setDistinguisher("captcha");
+		$captchaField->setRequired(true);
+		$captchaField->setSiteKey("6LfGBUEUAAAAAIC4spvBe8kIKhQlU_JsAVuTfnid");
+		$captchaField->setSecretKey(Secrets::LOGIN_CAPTCHA_SECRET);
+		$captchaField->addError(90106, ErrorCodes::ERR_90106);
+		$captchaField->setMissingErrorCode(90106);
+		$captchaField->addError(90107, ErrorCodes::ERR_90107);
+		$captchaField->setInvalidErrorCode(90107);
+		$form->addField($captchaField);
+
+		return $form;
+	}
+}
