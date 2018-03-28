9a2c67d46a20f9db39909c689e8965a1cb4eb0cf
Fauxil Fox <fauxil_fox@furmail.net>
2018-03-28 19:52:43 -0400

    remove old subform draggable invokation, do it in a for loop now for compatablity with multi-row subform
    This allows for a few things:
    - not being able to drag entries across subforms (could have caused some really weird stuff haha)
    - subforms with individual containers to be movable throughout

--- js/modules/subform_entry_field.js
+++ js/modules/subform_entry_field.js
@@ -8,10 +8,21 @@ $(document).on("click", ".add-sub-container-field-btn", function(e) {
 	$(this).prev().append($("<div></div>").addClass("subform-entry-sub-container col s12"));
 });
 try {
-	new Draggable.Sortable(document.querySelectorAll(".subform-entry-container"), {
-		draggable: <?= json_encode('.'.SubformMultipleEntryField::ENTRY_ITEM) ?>,
-		appendTo: 'body'
-	}).on("mirror:created", function(e) {
-		$(e.mirror).width($(e.originalSource).width()).find(".<?= SubformMultipleEntryField::REMOVE_BUTTON_CLASS ?>").remove();
-	});
+	for (var i = $(".subform-entry-container").length - 1; i >= 0; i--) {
+		if ($($(".subform-entry-container")[i]).find(".subform-entry-sub-container").length > 0) {
+			new Draggable.Sortable($($(".subform-entry-container")[i]).find(".subform-entry-sub-container").get(), {
+				draggable: <?= json_encode('.'.SubformMultipleEntryField::ENTRY_ITEM) ?>,
+				appendTo: '.subform-entry-container'
+			}).on("mirror:created", function(e) {
+				$(e.mirror).css("max-width", $(e.originalSource).width()).find(".<?= SubformMultipleEntryField::REMOVE_BUTTON_CLASS ?>").remove();
+			});
+		} else {
+			new Draggable.Sortable($(".subform-entry-container")[i], {
+				draggable: <?= json_encode('.'.SubformMultipleEntryField::ENTRY_ITEM) ?>,
+				appendTo: '.subform-entry-container'
+			}).on("mirror:created", function(e) {
+				$(e.mirror).css("max-width", $(e.originalSource).width()).find(".<?= SubformMultipleEntryField::REMOVE_BUTTON_CLASS ?>").remove();
+			});
+		}
+	}
 } catch(e) {}
