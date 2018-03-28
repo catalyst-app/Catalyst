8678be9ffb05df4f5daa52e151309902e7d79b30
Fauxil Fox <fauxil_fox@furmail.net>
2018-03-28 16:28:26 -0400

    add js for adding more subcontainers

--- js/modules/subform_entry_field.js
+++ js/modules/subform_entry_field.js
@@ -4,6 +4,9 @@ use \Catalyst\Form\Field\SubformMultipleEntryField;
 $(document).on("click", ".<?= SubformMultipleEntryField::REMOVE_BUTTON_CLASS ?>", function(e) {
 	$(this).closest(<?= json_encode('.'.SubformMultipleEntryField::ENTRY_ITEM) ?>).remove();
 });
+$(document).on("click", ".add-sub-container-field-btn", function(e) {
+	$(this).prev().append($("<div></div>").addClass("subform-entry-sub-container col s12"));
+});
 try {
 	new Draggable.Sortable(document.querySelectorAll(".subform-entry-container"), {
 		draggable: <?= json_encode('.'.SubformMultipleEntryField::ENTRY_ITEM) ?>,
