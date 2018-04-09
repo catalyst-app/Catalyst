b7d3c8b887ad8940847ea3080c780c2ef2b3c537
Fauxil Fox <fauxil@catalystapp.co>
2018-04-09 12:27:00 -0400

    proper right bar contents in nct form

--- includes/CommissionType/NewCommissionTypeFormTrait.php
+++ includes/CommissionType/NewCommissionTypeFormTrait.php
@@ -240,7 +240,7 @@ trait NewCommissionTypeFormTrait {
 		$modifiersField->setRequired(false);
 		$modifiersField->setLabel("Modifiers (these can be added to a commission like pizza toppings to a pizza order)");
 		$modifiersField->setDisplayHtml('<a href="#" class="'.SubformMultipleEntryField::ENTRY_ITEM.' btn commission-type-mod"><i class="material-icons right '.SubformMultipleEntryField::REMOVE_BUTTON_CLASS.'">clear</i>{modifier-psuedo-field} (+{base-cost-psuedo-field})</div>');
-		$modifiersField->setRightBarContents('<i class="material-icons right '.SubformMultipleEntryFieldWithRows::REMOVE_CONTAINER_BUTTON_CLASS.'">{uniq}clear</i>');
+		$modifiersField->setRightBarContents('<p class="inline-block no-bottom-margin"><label for="{uniq}"><input type="checkbox" class="filled-in" id="{uniq}"><span>Multiple</span></label></p><i class="material-icons '.SubformMultipleEntryFieldWithRows::REMOVE_CONTAINER_BUTTON_CLASS.'">clear</i>');
 
 		$modifierEntryWrapper = new WrappedField();
 		$modifierEntryWrapper->setWrapperClasses("col s12");
