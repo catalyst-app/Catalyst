559d775ead35be439d9b684ffd888b580424c501
Fauxil Fox <fauxil_fox@furmail.net>
2018-04-09 01:36:50 -0400

    again

--- includes/Artist/Artist.php
+++ includes/Artist/Artist.php
@@ -183,7 +183,7 @@ class Artist {
 	 */
 	public function getCurrentTosWithoutDate() : string {
 		if (array_key_exists("TOS", $this->cache)) {
-			trigger_error(json_encode($this->cache["TOS"]), E_USER_ERROR);
+			trigger_error($this->getColumnFromDatabase("TOS"), E_USER_ERROR);
 			return $this->cache["TOS"][0][1];
 		}
 
