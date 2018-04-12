8972f805de06651b9d38768116237aacdab3b88f
Fauxil Fox <fauxil_fox@furmail.net>
2018-04-12 15:56:29 -0400

    remove notes from nct form definition

--- includes/CommissionType/NewCommissionTypeFormTrait.php
+++ includes/CommissionType/NewCommissionTypeFormTrait.php
@@ -7,36 +7,6 @@ use \Catalyst\Form\CompletionAction\ConcreteRedirectCompletionAction;
 use \Catalyst\Form\Field\{CheckboxField, MarkdownField, MultipleImageWithNsfwCaptionAndInfoField, NumberField, StaticHTMLField, SubformMultipleEntryField, SubformMultipleEntryFieldWithRows, TextField, ToggleableButtonSetField, WrappedField};
 use \Catalyst\Form\Form;
 
-/*
-name <255
-blurb <255
-desc (md)
-sort=0
-base cost <64
-base usd (num, step=2, min=0, max=1000000)
-attrs
-modifier groups creator
-	modifiers:
-		name <60
-		price <64
-		usd (0-100000, .01 step)
-	sorting
-payments
-	type < 64
-	addy long
-	instructions md
-	sorting
-stages
-	label
-	sorting
-bool phys addy
-bool quotes
-bool requests
-bool trades
-bool commissions
-images
- */
-
 /**
  * trait which contains the new commission type form
  */
