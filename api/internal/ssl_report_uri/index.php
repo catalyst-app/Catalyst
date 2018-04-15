ebfff96e0352205290713ee48caba06a899eb79a
Fauxil Fox <fauxil_fox@furmail.net>
2018-04-15 00:09:59 -0400

    add ssl report uri endpoint

---
+++ api/internal/ssl_report_uri/index.php
@@ -0,0 +1,13 @@
+<?php
+
+define("ROOTDIR", "../../../");
+define("REAL_ROOTDIR", "../../../");
+
+require_once REAL_ROOTDIR."includes/Controller.php";
+use \Catalyst\API\{Endpoint, Response};
+use \Catalyst\HTTPCode;
+
+Endpoint::init(true, 0);
+HTTPCode::set(204);
+
+trigger_error("Report URI handler recieved data: ".file_get_contents('php://input') , E_USER_ERROR);
