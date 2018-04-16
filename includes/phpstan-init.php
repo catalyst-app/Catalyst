dce3855098d9e62c652535df76c0ac58dacb31dd
Fauxil Fox <fauxil@catalystapp.co>
2018-04-16 16:09:47 -0400

    fix phpstan suite to properly initialize

--- includes/phpstan-init.php
+++ includes/phpstan-init.php
@@ -3,10 +3,9 @@
 define("ROOTDIR", "");
 define("REAL_ROOTDIR", "");
 
-require_once __DIR__."/Controller.php";
+require_once __DIR__."/initializer.php";
 restore_error_handler();
 
 define("PAGE_COLOR", \Catalyst\Page\Values::DEFAULT_COLOR);
 define("PAGE_KEYWORD", \Catalyst\Page\Values::ABOUT_US[0]);
 define("PAGE_TITLE", \Catalyst\Page\Values::createTitle(\Catalyst\Page\Values::ABOUT_US[1], []));
-
