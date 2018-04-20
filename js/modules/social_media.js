f2f97db6e2b0cf37048e01075154e499de8e1d39
Fauxil Fox <fauxil@catalystapp.co>
2018-04-20 09:26:30 -0400

    refactor the remainder of social media js, add logging where needed

--- js/modules/social_media.js
+++ js/modules/social_media.js
@@ -38,7 +38,7 @@ $(document).on("click", ".social-chips .chip i", function(e) {
 		window.log(<?= json_encode(basename(__FILE__)) ?>, ".on click .social-chip .chip i - request complete, toasting");
 		M.escapeToast("Removed", 4000);
 	}).fail(function(response) {
-		window.log(<?= json_encode(basename(__FILE__)) ?>, ".on click .social-chip .chip i - request failed, parsing error and showing toast", true);
+		window.log(<?= json_encode(basename(__FILE__)) ?>, ".on click .social-chip .chip i - request failed, parsing error and toasting", true);
 		console.log(response);
 		var data = JSON.parse(response.responseText);
 		showErrorMessageForCode(data.error_code);
@@ -51,41 +51,37 @@ $(document).on("click", ".social-chips .chip i", function(e) {
 });
 
 /* MOVE SOCIAL MEDIA */
-try {
-	new Draggable.Sortable(document.querySelector('.social-chips-editable.social-chips > div'), {
-		draggable: '.social-chips-editable.social-chips > div > a, .social-chips-editable.social-chips > div > .chip',
-		appendTo: 'body'
-	}).on("mirror:created", function(e) {
-		$(e.mirror).css("max-width", $(e.originalSource).width());
-		$(e.mirror).find("i").remove(); // dont show remove button while dragging
-	}).on("sortable:stop", function() {
-		var result = [];
-		for (var i = 0; i < $(".social-chips .chip:not(.draggable-mirror):visible").length; i++) {
-			result.push($($(".social-chips .chip:not(.draggable-mirror):visible")[i]).attr("data-id"));
-		}
-		
-		var data = new FormData();
-		data.append("rootdir", $("html").attr("data-rootdir"));
-		if ($("#social-dest-type").length !== 1) {
-			data.append("dest", "");
-		} else {
-			data.append("dest", $("#social-dest-type").val());
-		}
-		data.append("order", JSON.stringify(result));
+new Draggable.Sortable(document.querySelector('.social-chips-editable.social-chips > div'), {
+	draggable: '.social-chips-editable.social-chips > div > a, .social-chips-editable.social-chips > div > .chip',
+	appendTo: 'body'
+}).on("mirror:created", function(e) {
+	window.log(<?= json_encode(basename(__FILE__)) ?>, "mirror:created - constraining mirror width to original, removing icon");
+	$(e.mirror).css("max-width", $(e.originalSource).width());
+	$(e.mirror).find("i").remove(); // dont show remove button while dragging
+}).on("sortable:stop", function() {
+	var result = [];
+	for (var i = 0; i < $(".social-chips .chip:not(.draggable-mirror):visible").length; i++) {
+		result.push($($(".social-chips .chip:not(.draggable-mirror):visible")[i]).attr("data-id"));
+	}
+	
+	window.log(<?= json_encode(basename(__FILE__)) ?>, "sortable:stop - aggregating chip order, saving to server ("+JSON.stringify(result)+")");
 
-		$.ajax($("html").attr("data-rootdir") + "api\/internal\/social_media\/order\/", {
-			data: data,
-			processData: false,
-			contentType: false,
-			method: "POST"
-		}).done(function(response) {
-			console.log(response);
-			var data = JSON.parse(response);
-			M.escapeToast("Saved", 4000);
-		}).fail(function(response) {
-			console.log(response);
-			var data = JSON.parse(response.responseText);
-			showErrorMessageForCode(data.error_code);
-		});
+	var data = new FormData();
+	data.append("rootdir", $("html").attr("data-rootdir"));
+	data.append("dest", $("#social-dest-type").val());
+	data.append("order", JSON.stringify(result));
+
+	$.ajax($("html").attr("data-rootdir") + "api\/internal\/social_media\/order\/", {
+		data: data,
+		processData: false,
+		contentType: false,
+		method: "POST"
+	}).done(function(response) {
+		window.log(<?= json_encode(basename(__FILE__)) ?>, "sortable:stop - request complete, toasting");
+		M.escapeToast("Saved", 4000);
+	}).fail(function(response) {
+		window.log(<?= json_encode(basename(__FILE__)) ?>, "sortable:stop - request failed, parsing error and toasting", true);
+		var data = JSON.parse(response.responseText);
+		showErrorMessageForCode(data.error_code);
 	});
-} catch(e) {}
+});
