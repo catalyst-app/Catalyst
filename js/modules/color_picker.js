44956498873dbb4d73b1dd341fe5cf8148879f73
Fauxil Fox <fauxil_fox@furmail.net>
2018-04-20 07:00:44 -0400

    use new semantics for logging in color picker js

--- js/modules/color_picker.js
+++ js/modules/color_picker.js
@@ -14,7 +14,7 @@ function initializeColorPicker() {
 		}, 100);
 	}
 
-	window.log(<?= json_encode(basename(__FILE__)) ?>, "Color picker initialized");
+	window.log(<?= json_encode(basename(__FILE__)) ?>, "initializeColorPicker - color picker initialized");
 }
 
 $(document).on("click", ".color-field", function(e) {
@@ -34,7 +34,7 @@ $(document).on("click", ".color-swatch", function(e) {
 		backgroundColor: $(this).css("backgroundColor")
 	}, 200);
 
-	window.log(<?= json_encode(basename(__FILE__)) ?>, "Color "+$(this).css("backgroundColor")+" chosen");
+	window.log(<?= json_encode(basename(__FILE__)) ?>, ".on click for .color-swatch - color "+$(this).css("backgroundColor")+" chosen");
 
 	if (Array.isArray(colors)) {
 		for (var i = 0; i < colors.length; i++) {
@@ -43,9 +43,9 @@ $(document).on("click", ".color-swatch", function(e) {
 			}, 200);
 		}
 
-		window.log(<?= json_encode(basename(__FILE__)) ?>, "Color swatches updated");
+		window.log(<?= json_encode(basename(__FILE__)) ?>, ".on click for .color-swatch - color swatches updated");
 	} else {
 		$(".color-picker-modal").modal("close");
-		window.log(<?= json_encode(basename(__FILE__)) ?>, "Modal closed");
+		window.log(<?= json_encode(basename(__FILE__)) ?>, ".on click for .color-swatch - modal closed");
 	}
 });
