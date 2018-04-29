4fa268c2ba634ec13931e2fa5f616b0de87a495d
Fauxil Fox <fauxil_fox@furmail.net>
2018-04-29 15:28:46 -0400

    bump draggable version

--- src/Page/Header/Header.php
+++ src/Page/Header/Header.php
@@ -12,17 +12,15 @@ class Header {
 	 * JS libraries n such
 	 */
 	protected const SCRIPTS = [
-		// jQuery may be able to drop once materialize does
 		["https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js", 'crossorigin="anonymous"'],
-		// migrate to 1.0.0 alpha?
 		["https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.js", 'crossorigin="anonymous"'],
-		// best i've found so far, works well
-		["https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.6/draggable.bundle.legacy.js", 'crossorigin="anonymous"'],
+		["https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.7/draggable.bundle.legacy.js", 'crossorigin="anonymous"'],
 		// not vital
 		["https://cdn.rawgit.com/namuol/cheet.js/master/cheet.min.js", "defer", 'crossorigin="anonymous"'],
 		// crossorigin doesn't like to behave here
 		["https://google.com/recaptcha/api.js", "defer"],
 		
+		// we want this to load firstestest
 		[ROOTDIR."js/modules/error_handler.js?{commit}"],
 		
 		[ROOTDIR."js/modules/markdown_parser.js?{commit}", "defer"],
