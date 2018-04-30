86df6de0da50a6523dcc37b9edc3728c4fe4f0b3
Fauxil Fox <fauxil@catalystapp.co>
2018-04-30 09:58:37 -0400

    add class constants for dev/prod/always flags

--- src/Page/Header/Header.php
+++ src/Page/Header/Header.php
@@ -8,41 +8,45 @@ use Catalyst\Controller;
  * Used to hold data about what scripts and other resources should belong in the <head> element
  */
 class Header {
+	const PRODUCTION = 0;
+	const DEVEL = 1;
+	const ALWAYS = 2;
+
 	/**
 	 * JS libraries n such
 	 */
 	protected const SCRIPTS = [
 		// use minified versions in production
-		[1, "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js", 'crossorigin="anonymous"'],
-		[0, "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js", 'crossorigin="anonymous"'],
+		[self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js", 'crossorigin="anonymous"'],
+		[self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js", 'crossorigin="anonymous"'],
 		
-		[1, "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.js", 'crossorigin="anonymous"'],
-		[0, "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js", 'crossorigin="anonymous"'],
+		[self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.js", 'crossorigin="anonymous"'],
+		[self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js", 'crossorigin="anonymous"'],
 		
-		[1, "https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.6/draggable.bundle.legacy.js", 'crossorigin="anonymous"'],
-		[0, "https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.6/draggable.bundle.legacy.min.js", 'crossorigin="anonymous"'],
+		[self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.6/draggable.bundle.legacy.js", 'crossorigin="anonymous"'],
+		[self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.6/draggable.bundle.legacy.min.js", 'crossorigin="anonymous"'],
 		
 		// not vital, easter egg
-		[1, "https://cdn.rawgit.com/namuol/cheet.js/master/cheet.js", "defer", 'crossorigin="anonymous"'],
-		[0, "https://cdn.rawgit.com/namuol/cheet.js/master/cheet.min.js", "defer", 'crossorigin="anonymous"'],
+		[self::DEVEL, "https://cdn.rawgit.com/namuol/cheet.js/master/cheet.js", "defer", 'crossorigin="anonymous"'],
+		[self::PRODUCTION, "https://cdn.rawgit.com/namuol/cheet.js/master/cheet.min.js", "defer", 'crossorigin="anonymous"'],
 		
 		// crossorigin doesn't like to behave here
 		// already comes minified
-		[2, "https://google.com/recaptcha/api.js", "defer"],
+		[self::ALWAYS, "https://google.com/recaptcha/api.js", "defer"],
 		
 		// we want this to load firstestest
-		[1, ROOTDIR."js/modules/error_handler.js?{commit}"],
+		[self::DEVEL, ROOTDIR."js/modules/error_handler.js?{commit}"],
 		
-		[1, ROOTDIR."js/modules/markdown_parser.js?{commit}", "defer"],
-		[1, ROOTDIR."js/modules/ajax_progress.js?{commit}", "defer"],
-		[1, ROOTDIR."js/modules/console_message.js?{commit}", "defer"],
-		[1, ROOTDIR."js/modules/color_functions.js?{commit}", "defer"],
-		[1, ROOTDIR."js/modules/input_functions.js?{commit}", "defer"],
-		[1, ROOTDIR."js/modules/polyfills.js?{commit}", "defer"],
-		[1, ROOTDIR."js/modules/totp_preview.js?{commit}", "defer"],
-		[1, ROOTDIR."js/modules/onload.js?{commit}"],
+		[self::DEVEL, ROOTDIR."js/modules/markdown_parser.js?{commit}", "defer"],
+		[self::DEVEL, ROOTDIR."js/modules/ajax_progress.js?{commit}", "defer"],
+		[self::DEVEL, ROOTDIR."js/modules/console_message.js?{commit}", "defer"],
+		[self::DEVEL, ROOTDIR."js/modules/color_functions.js?{commit}", "defer"],
+		[self::DEVEL, ROOTDIR."js/modules/input_functions.js?{commit}", "defer"],
+		[self::DEVEL, ROOTDIR."js/modules/polyfills.js?{commit}", "defer"],
+		[self::DEVEL, ROOTDIR."js/modules/totp_preview.js?{commit}", "defer"],
+		[self::DEVEL, ROOTDIR."js/modules/onload.js?{commit}"],
 
-		[0, ROOTDIR."js/dist/packed.min.js"],
+		[self::PRODUCTION, ROOTDIR."js/dist/packed.min.js?{commit}"],
 	];
 
 	/**
@@ -52,21 +56,21 @@ class Header {
 	public static function getScripts() : array {
 		$scripts = [];
 		foreach (self::SCRIPTS as $script) {
-			switch ($script[0]) {
-				case 0: // production only
+			switch ($script[self::PRODUCTION]) {
+				case self::PRODUCTION: // production only
 					if (Controller::isDevelMode()) {
 						continue 2;
 					}
 					break;
-				case 1: // devel only
+				case self::DEVEL: // devel only
 					if (!Controller::isDevelMode()) {
 						continue 2;
 					}
 					break;
-				case 2:
+				case self::ALWAYS:
 				break;
 			}
-			$script[1] = str_replace("{commit}", Controller::getCommit(), $script[1]);
+			$script[self::DEVEL] = str_replace("{commit}", Controller::getCommit(), $script[self::DEVEL]);
 			$scripts[] = array_slice($script, 1);
 		}
 		return $scripts;
@@ -77,17 +81,17 @@ class Header {
 	 */
 	protected const STYLES = [
 		// materialize main
-		[1, "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.css"],
-		[0, "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css"],
+		[self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.css"],
+		[self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css"],
 
 		// icon set + robotos
-		[2, "https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Material+Icons"],
+		[self::ALWAYS, "https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Material+Icons"],
 		
 		// overall styles and such, mostly just small things
-		[2, ROOTDIR."css/overall.css?{commit}"],
+		[self::ALWAYS, ROOTDIR."css/overall.css?{commit}"],
 		
 		// used for styling, uses rewrite color-######.css to get the approprite styles
-		[2, ROOTDIR."css/color-".PAGE_COLOR.".css?{commit}"],
+		[self::ALWAYS, ROOTDIR."css/color-".PAGE_COLOR.".css?{commit}"],
 	];
 
 	/**
@@ -97,21 +101,21 @@ class Header {
 	public static function getStyles() : array {
 		$styles = [];
 		foreach (self::STYLES as $style) {
-			switch ($style[0]) {
-				case 0: // production only
+			switch ($style[self::PRODUCTION]) {
+				case self::PRODUCTION: // production only
 					if (Controller::isDevelMode()) {
 						continue 2;
 					}
 					break;
-				case 1: // devel only
+				case self::DEVEL: // devel only
 					if (!Controller::isDevelMode()) {
 						continue 2;
 					}
 					break;
-				case 2:
+				case self::ALWAYS:
 				break;
 			}
-			$styles[] = str_replace("{commit}", Controller::getCommit(), $style[1]);
+			$styles[] = str_replace("{commit}", Controller::getCommit(), $style[self::DEVEL]);
 		}
 		return $styles;
 	}
