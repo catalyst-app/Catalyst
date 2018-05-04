4840257250898597a04dd088611a351d99e6f51f
Fauxil Fox <fauxil@catalystapp.co>
2018-05-04 13:11:27 -0400

    add some more fucking crypto libraries

--- src/Page/Header/Header.php
+++ src/Page/Header/Header.php
@@ -26,11 +26,11 @@ class Header {
 		[self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.7/draggable.bundle.legacy.js", 'crossorigin="anonymous"'],
 		[self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.7/draggable.bundle.legacy.min.js", 'crossorigin="anonymous"'],
 
-		// [self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/jsencrypt/2.3.1/jsencrypt.js", 'crossorigin="anonymous"'],
-		// [self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/jsencrypt/2.3.1/jsencrypt.min.js", 'crossorigin="anonymous"'],
+		[self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/jsencrypt/2.3.1/jsencrypt.js", 'crossorigin="anonymous"'],
+		[self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/jsencrypt/2.3.1/jsencrypt.min.js", 'crossorigin="anonymous"'],
 
-		// [self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/aes-js/3.1.1/index.js", 'crossorigin="anonymous"'],
-		// [self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/aes-js/3.1.1/index.min.js", 'crossorigin="anonymous"'],
+		[self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/aes-js/3.1.1/index.js", 'crossorigin="anonymous"'],
+		[self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/aes-js/3.1.1/index.min.js", 'crossorigin="anonymous"'],
 
 		[self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/sjcl/1.0.7/sjcl.js", 'crossorigin="anonymous"'],
 		[self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/sjcl/1.0.7/sjcl.min.js", 'crossorigin="anonymous"'],
