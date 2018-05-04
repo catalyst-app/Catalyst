00ad3ae3c50de784fca96bdb48fb964815240acf
Fauxil Fox <fauxil@catalystapp.co>
2018-05-03 20:36:20 -0400

    add jsencrypt library

--- src/Page/Header/Header.php
+++ src/Page/Header/Header.php
@@ -25,6 +25,9 @@ class Header {
 		
 		[self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.7/draggable.bundle.legacy.js", 'crossorigin="anonymous"'],
 		[self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/draggable/1.0.0-beta.7/draggable.bundle.legacy.min.js", 'crossorigin="anonymous"'],
+
+		[self::DEVEL, "https://cdnjs.cloudflare.com/ajax/libs/jsencrypt/2.3.1/jsencrypt.js", 'crossorigin="anonymous"'],
+		[self::PRODUCTION, "https://cdnjs.cloudflare.com/ajax/libs/jsencrypt/2.3.1/jsencrypt.min.js", 'crossorigin="anonymous"'],
 		
 		// not vital, easter egg
 		[self::DEVEL, "https://cdn.rawgit.com/namuol/cheet.js/master/cheet.js", "defer", 'crossorigin="anonymous"'],
