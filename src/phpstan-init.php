870f8694b1da84ca66c91320d81210aa721c627f
Fauxil Fox <fauxil_fox@furmail.net>
2018-05-17 13:15:20 -0400

    add additional phpstan config settings

--- src/phpstan-init.php
+++ src/phpstan-init.php
@@ -4,8 +4,11 @@ define("ROOTDIR", "");
 define("REAL_ROOTDIR", "");
 
 require_once __DIR__."/initializer.php";
+use \Catalyst\User\User;
 restore_error_handler();
 
 define("PAGE_COLOR", \Catalyst\Page\Values::DEFAULT_COLOR);
 define("PAGE_KEYWORD", \Catalyst\Page\Values::ABOUT_US[0]);
 define("PAGE_TITLE", \Catalyst\Page\Values::createTitle(\Catalyst\Page\Values::ABOUT_US[1], []));
+
+$_SESSION["user"] = new User(1);
