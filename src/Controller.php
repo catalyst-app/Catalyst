ba1815f1c13e9f3318fff022acd75088a7d250b0
Fauxil Fox <fauxil_fox@furmail.net>
2018-05-16 20:12:41 -0400

    fix improper argv display in CLI usage

--- src/Controller.php
+++ src/Controller.php
@@ -255,7 +255,7 @@ class Controller {
 				"  ERRFILE:    ".$errfile."\n".
 				"  ERRLINE:    ".$errline."\n".
 				"  TRACK_ID:    ".$trackingId."\n".
-				"  ARGV:   ".$_SERVER["argv"]."\n";
+				"  ARGV:   ".implode(" | ", $_SERVER["argv"])."\n";
 			flush();
 			exit(1); // non-zero error codes ye
 		}
