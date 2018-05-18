60e665fa689f1f6914bafb6ad4444599d60420ee
Fauxil Fox <fauxil_fox@furmail.net>
2018-05-18 14:31:35 -0400

    add initial data to database model

--- src/Database/AbstractDatabaseModel.php
+++ src/Database/AbstractDatabaseModel.php
@@ -27,13 +27,26 @@ abstract class AbstractDatabaseModel implements Serializable {
 	protected static $cache = [];
 
 	/**
+	 * Used to cache pending updates so we can update multiple columns at once
+	 * @var array
+	 */
+	protected $pendingUpdates = [];
+
+	/**
+	 * Used to cache pending updates so we can update multiple columns at once
+	 * @var self[]
+	 */
+	protected static $objectsPendingUpdates = [];
+
+	/**
 	 * Creates a new object
 	 *
 	 * @param int $id
 	 * @param array $cache Prefilled cache
 	 */
 	public function __construct(int $id, array $cache=[]) {
-		if (!self::idExists($id)) {
+		$row = self::getInitialData($id);
+		if ($row === false) {
 			throw new InvalidArgumentException("ID ".$id." does not exist in table ".static::getTable().".");
 		}
 		$this->id = $id;
@@ -47,16 +60,23 @@ abstract class AbstractDatabaseModel implements Serializable {
 	}
 
 	/**
-	 * Check if a given ID exists in the database
+	 * Check if a given ID exists in the database.  If so, get prefilled data
 	 * 
 	 * @param int $id
-	 * @return bool
+	 * @return bool|array
 	 */
-	public static function idExists(int $id) : bool {
+	public static function getInitialData(int $id) {
 		$stmt = new SelectQuery();
 		
 		$stmt->setTable(static::getTable());
-		$stmt->addColumn(new Column("ID", static::getTable()));
+
+		$columns = static::getPrefetchColumns();
+		$columns[] = "ID";
+		$columns = array_unique($columns);
+
+		foreach ($columns as $column) {
+			$stmt->addColumn(new Column($column, static::getTable()));
+		}
 
 		$whereClause = new WhereClause();
 		$whereClause->addToClause([new Column("ID", static::getTable()), "=", $id]);
@@ -68,8 +88,13 @@ abstract class AbstractDatabaseModel implements Serializable {
 		if (count($stmt->getResult()) == 0) {
 			return false;
 		}
-		return true;
+		return $stmt->getResult()[0];
 	}
+
+	/**
+	 * Get list of columns to prefetch
+	 */
+	abstract public static function getPrefetchColumns() : array;
 
 	/**
 	 * Get the table in which the object's data is stored in
@@ -118,7 +143,33 @@ abstract class AbstractDatabaseModel implements Serializable {
 	protected function getColumnFromDatabaseOrCache(string $column) {
 		if (array_key_exists($column, self::$cache[static::class][$this->id])) {
 			return self::$cache[static::class][$this->id][$column];
+		}
 		return self::$cache[static::class][$this->id][$column] = $this->getColumnFromDatabase($column);
+	}
+
+	/**
+	 * Prefetch database columns we know we will be using
+	 * 
+	 * @param string[] $columns Columns to get
+	 */
+	protected function prefetchColumns(array $columns) : void {
+		$stmt = new SelectQuery();
+
+		$stmt->setTable($this->getTable());
+
+		foreach ($columns as $column) {
+			$stmt->addColumn(new Column($column, $this->getTable()));
+		}
+
+		$whereClause = new WhereClause();
+		$whereClause->addToClause([new Column("ID", $this->getTable()), "=", $this->getId()]);
+
+		$stmt->addAdditionalCapability($whereClause);
+
+		$stmt->execute();
+
+		foreach ($stmt->getResult()[0] as $column => $value) {
+			self::$cache[static::class][$this->id][$column] = $value;
 		}
 	}
 
@@ -153,13 +204,46 @@ abstract class AbstractDatabaseModel implements Serializable {
 	/**
 	 * Updates a column in the database
 	 */
-	protected function updateColumnInDatabase(string $column, $value) : void {
+	protected function updateColumnInDatabase(string $column, $value, bool $writeImmediately=false) : void {
+		if ($writeImmediately) {
+			$stmt = new UpdateQuery();
+
+			$stmt->setTable($this->getTable());
+
+			$stmt->addColumn(new Column($column, $this->getTable()));
+			$stmt->addValue($value);
+
+			$whereClause = new WhereClause();
+			$whereClause->addToClause([new Column("ID", $this->getTable()), "=", $this->getId()]);
+			$stmt->addAdditionalCapability($whereClause);
+
+			$stmt->execute();
+		} else {
+			// using key => value allows easy and painless overwriting if the same value is changed twice
+			$this->pendingUpdates[$column] = $value;
+
+			// for shutdown
+			self::$objectsPendingUpdates[] = $this;
+		}
+
+		self::$cache[static::class][$this->id][$column] = $value;
+	}
+
+	/**
+	 * Write updates to the database
+	 */
+	public function writeUpdates() : void {
+		if (empty($this->pendingUpdates)) {
+			return;
+		}
 		$stmt = new UpdateQuery();
 
 		$stmt->setTable($this->getTable());
 
-		$stmt->addColumn(new Column($column, $this->getTable()));
-		$stmt->addValue($value);
+		foreach ($this->pendingUpdates as $key => $value) {
+			$stmt->addColumn(new Column($key, $this->getTable()));
+			$stmt->addValue($value);
+		}
 
 		$whereClause = new WhereClause();
 		$whereClause->addToClause([new Column("ID", $this->getTable()), "=", $this->getId()]);
@@ -167,7 +251,16 @@ abstract class AbstractDatabaseModel implements Serializable {
 
 		$stmt->execute();
 
-		$this->cache[$column] = $value;
+		$this->pendingUpdates = [];
+	}
+
+	/**
+	 * Write the updates of all models pending updates
+	 */
+	public static function writeAllUpdates() : void {
+		foreach (self::$objectsPendingUpdates as $obj) {
+			$obj->writeUpdates();
+		}
 	}
 
 	/**
