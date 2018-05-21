6d7b917ff2615c6319e6711e7e324ab4c6fe4b4f
Fauxil Fox <fauxil_fox@furmail.net>
2018-05-21 12:00:48 -0400

    make commission type stage implement abstractdatabasemodel

--- src/CommissionType/CommissionTypeStage.php
+++ src/CommissionType/CommissionTypeStage.php
@@ -2,51 +2,88 @@
 
 namespace Catalyst\CommissionType;
 
+use \Catalyst\Database\{AbstractDatabaseModel, Column, Tables};
+use \Catalyst\Database\Query\InsertQuery;
+
 /**
  * Represents a commission type stage
  *
  * Basic model class, nothing fancy
+ * @method int getCommissionTypeId()
+ * @method void setCommissionTypeId(int $commissionTypeId)
+ * @method string getStage()
+ * @method void setStage(string $stage)
+ * @method bool getDeleted()
+ * @method void setDeleted(bool $deleted)
  */
-class CommissionTypeStage {
+class CommissionTypeStage extends AbstractDatabaseModel {
 	/**
-	 * @var int
-	 */
-	protected $id = 0;
-	/**
-	 * @var string
-	 */
-	protected $stage = "";
-
-	/**
-	 * Constructor
+	 * Columns to prefetch from constructor
 	 *
-	 * @param int $id
-	 * @param string $stage
+	 * @return string[]
 	 */
-	public function __construct(int $id, string $stage="") {
-		$this->id = $id;
-		$this->setStage($stage);
-	}
-
-
-	/**
-	 * @return int
-	 */
-	public function getId() : int {
-		return $this->id;
+	public static function getPrefetchColumns() : array {
+		return [
+			"COMMISSION_TYPE_ID",
+			"STAGE",
+			"DELETED",
+		];
 	}
 
 	/**
+	 * Table in which data resides in
+	 *
 	 * @return string
 	 */
-	public function getStage() : string {
-		return $this->stage;
+	public static function getTable() : string {
+		return Tables::COMMISSION_TYPE_STAGES;
 	}
 
 	/**
-	 * @param string $stage
+	 * Get values to set upon deletion
+	 *
+	 * We don't delete any information such as name/etc because artist's will need this information in the future for previous commissions
+	 * No [Deleted] because these are moved around and that may cause things to be weird
+	 * @return array
 	 */
-	public function setStage(string $stage) : void {
-		$this->stage = $stage;
+	public function getDeletedValues() : array {
+		return [
+			"DELETED" => true,
+		];
+	}
+
+	/**
+	 * Create a new CommissionTypePaymentOption from the given info
+	 *
+	 * @param array $values
+	 * @return self
+	 */
+	public static function create(array $values) : self {
+		// no prefilling is available as this is such a low-level component
+		$stmt = new InsertQuery();
+
+		$stmt->setTable(self::getTable());
+
+		foreach (["COMMISSION_TYPE_ID", "STAGE"] as $column) {
+			$stmt->addColumn(new Column($column, self::getTable()));
+			$stmt->addValue($values[$column]);
+		}
+
+		$stmt->execute();
+
+		return new self($stmt->getResult(), $values, false);
+	}
+
+	/**
+	 * Easily modifiable properties of the object
+	 *
+	 * @return array
+	 */
+	public static function getModifiableProperties() : array {
+		return [
+			"CommissionTypeId" => ["COMMISSION_TYPE_ID", null, null],
+			"Stage" => ["STAGE", null, null],
+			"Deleted" => ["DELETED", "boolval", null],
+		];
 	}
 }
