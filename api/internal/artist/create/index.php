108062a1c00b00d6da6b3eecf17beac7589dc76e
Fauxil Fox <fauxil_fox@furmail.net>
2018-05-21 08:04:46 -0400

    refactor artist creation endpoint to new spec

--- api/internal/artist/create/index.php
+++ api/internal/artist/create/index.php
@@ -22,78 +22,27 @@ if ($_SESSION["user"]->isArtist() || $_SESSION["user"]->wasArtist()) {
 	Response::sendErrorResponse(91215, ErrorCodes::ERR_91215);
 }
 
-$stmt = new SelectQuery();
-
-$stmt->setTable(Tables::ARTIST_PAGES);
-
-$stmt->addColumn(new Column("ID", Tables::ARTIST_PAGES));
-
-$whereClause = new WhereClause();
-$whereClause->addToClause([new Column("URL", Tables::ARTIST_PAGES), '=', $_POST["url"]]);
-$stmt->addAdditionalCapability($whereClause);
-
-$stmt->execute();
-
-if (count($stmt->getResult())) {
-	HTTPCode::set(400);
-	Response::sendErrorResponse(91205, ErrorCodes::ERR_91205);
-}
-
-$stmt = new InsertQuery();
-
-$stmt->setTable(Tables::ARTIST_PAGES);
-
-$stmt->addColumn(new Column("USER_ID", Tables::ARTIST_PAGES));
-$stmt->addValue($_SESSION["user"]->getId());
-
 $token = Tokens::generateArtistToken();
-
-$stmt->addColumn(new Column("TOKEN", Tables::ARTIST_PAGES));
-$stmt->addValue($token);
-
-$stmt->addColumn(new Column("NAME", Tables::ARTIST_PAGES));
-$stmt->addValue($_POST["name"]);
-
-$stmt->addColumn(new Column("URL", Tables::ARTIST_PAGES));
-$stmt->addValue($_POST["url"]);
-
-$stmt->addColumn(new Column("DESCRIPTION", Tables::ARTIST_PAGES));
-$stmt->addValue($_POST["description"]);
-
-$tos = [date("l, F jS, Y"), $_POST["tos"]];
-$tos = json_encode([$tos]);
-
-$stmt->addColumn(new Column("TOS", Tables::ARTIST_PAGES));
-$stmt->addValue($tos);
 
 $image = null;
 if (isset($_FILES["image"])) {
-	$newImage = Image::upload($_FILES["image"], Folders::ARTIST_IMAGE, $token);
-	if (!is_null($newImage)) {
-		$image = $newImage->getPath();
-	}
+	$image = Image::upload($_FILES["image"], Folders::ARTIST_IMAGE, $token);
 }
-$stmt->addColumn(new Column("IMG", Tables::ARTIST_PAGES));
-$stmt->addValue($image);
 
-$stmt->addColumn(new Column("COLOR", Tables::ARTIST_PAGES));
-$stmt->addValue(hex2bin($_POST["color"]));
-
-$stmt->execute();
-
-$aid = $stmt->getResult();
-
-$stmt = new UpdateQuery();
-
-$stmt->setTable(Tables::USERS);
-
-$stmt->addColumn(new Column("ARTIST_PAGE_ID", Tables::USERS));
-$stmt->addValue($aid);
-
-$whereClause = new WhereClause();
-$whereClause->addToClause([new Column("ID", Tables::USERS), '=', $_SESSION["user"]->getId()]);
-$stmt->addAdditionalCapability($whereClause);
-
-$stmt->execute();
+Artist::create([
+	"USER_ID" => $_SESSION["user"]->getId(),
+	"TOKEN" => $token,
+	"NAME" => $_POST["name"],
+	"URL" => $_POST["url"],
+	"DESCRIPTION" => $_POST["description"],
+	"TOS" => json_encode([
+		[
+			date("l, F jS, Y"),
+			$_POST["tos"]
+		],
+	]),
+	"IMG" => !is_null($image) ? $image : null,
+	"COLOR" => hex2bin($_POST["color"]),
+]);
 
 Response::sendSuccessResponse("Success");
