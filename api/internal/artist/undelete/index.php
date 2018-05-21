62592694d3c3ec4b5ba8e4c92071a4a6f90c12d6
Fauxil Fox <fauxil_fox@furmail.net>
2018-05-21 09:12:48 -0400

    refactor artist undelete endpoint

--- api/internal/artist/undelete/index.php
+++ api/internal/artist/undelete/index.php
@@ -5,9 +5,7 @@ define("REAL_ROOTDIR", "../../../../");
 
 require_once REAL_ROOTDIR."src/initializer.php";
 use \Catalyst\API\{Endpoint, ErrorCodes, Response};
-use \Catalyst\Database\{Column, Tables};
-use \Catalyst\Database\QueryAddition\WhereClause;
-use \Catalyst\Database\Query\{SelectQuery, UpdateQuery};
+use \Catalyst\Artist\Artist;
 use \Catalyst\Form\FormRepository;
 use \Catalyst\HTTPCode;
 
@@ -19,51 +17,17 @@ if ($_SESSION["user"]->isArtist()) {
 	HTTPCode::set(400);
 	Response::sendErrorResponse(91101, ErrorCodes::ERR_91101);
 }
-if (!$_SESSION["user"]->wasArtist()) {
+
+$aid = Artist::getIdFromUserId($_SESSION["user"]->getId());
+
+if ($aid == -1) {
 	HTTPCode::set(400);
 	Response::sendErrorResponse(91102, ErrorCodes::ERR_91102);
 }
 
-$stmt = new UpdateQuery();
+$artist = new Artist($aid);
+$artist->setDeleted(false);
 
-$stmt->setTable(Tables::ARTIST_PAGES);
-
-$stmt->addColumn(new Column("DELETED", Tables::ARTIST_PAGES));
-$stmt->addValue(0);
-
-$whereClause = new WhereClause();
-$whereClause->addToClause([new Column("USER_ID", Tables::ARTIST_PAGES), '=', $_SESSION["user"]->getId()]);
-$stmt->addAdditionalCapability($whereClause);
-
-$stmt->execute();
-
-
-$stmt = new SelectQuery();
-
-$stmt->setTable(Tables::ARTIST_PAGES);
-
-$stmt->addColumn(new Column("ID", Tables::ARTIST_PAGES));
-
-$whereClause = new WhereClause();
-$whereClause->addToClause([new Column("USER_ID", Tables::ARTIST_PAGES), '=', $_SESSION["user"]->getId()]);
-$stmt->addAdditionalCapability($whereClause);
-
-$stmt->execute();
-
-$aid = $stmt->getResult()[0]["ID"];
-
-
-$stmt = new UpdateQuery();
-
-$stmt->setTable(Tables::USERS);
-
-$stmt->addColumn(new Column("ARTIST_PAGE_ID", Tables::USERS));
-$stmt->addValue($aid);
-
-$whereClause = new WhereClause();
-$whereClause->addToClause([new Column("ID", Tables::USERS), '=', $_SESSION["user"]->getId()]);
-$stmt->addAdditionalCapability($whereClause);
-
-$stmt->execute();
+$_SESSION["user"]->setArtistPageId($aid);
 
 Response::sendSuccessResponse("Success");
