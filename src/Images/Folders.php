a8d6432cbbf3b41b18487634a8357d7d14b31dc5
Fauxil Fox <fauxil_fox@furmail.net>
2018-05-28 21:58:09 -0400

    better whitespace in Folders

--- src/Images/Folders.php
+++ src/Images/Folders.php
@@ -10,6 +10,7 @@ class Folders {
 	public const CHARACTER_IMAGE = "character_images";
 	public const ARTIST_IMAGE = "artist_images";
 	public const COMMISSION_TYPE_IMAGE = "commission_type_images";
+
 	public const INTEGRATION_ICONS = "integration_icons";
 
 	public const GLOBAL_IMG = "img/placeholders";
