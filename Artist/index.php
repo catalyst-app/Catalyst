588ca335634228a07801034879ae65a364f07e39
Fauxil-Fox <fauxil@catalystapp.co>
2018-05-31 08:36:56 -0400

    further indentation fixes

--- Artist/index.php
+++ Artist/index.php
@@ -117,84 +117,84 @@ $commissionTypes = array_filter($commissionTypes, function($type) {
 					<?php $firstCommissionType = true; ?>
 					<?php foreach ($commissionTypes as $commissionType): ?>
 						<?php if (!$firstCommissionType): ?>
-						<div class="divider small-bottom-margin"></div>
+							<div class="divider small-bottom-margin"></div>
 						<?php endif; ?>
-							<div class="commission-type-row" id="ct-<?= htmlspecialchars($commissionType->getToken()) ?>" data-token="<?= htmlspecialchars($commissionType->getToken()) ?>">
-								<div class="commission-type-row-header">
-									<h5 class="inline-block"><?= htmlspecialchars($commissionType->getName()) ?></h5>
-									<?php if (User::isLoggedIn()): ?>
-										<?php foreach (["hide-on-small-only horizontal", "hide-on-med-and-up"] as $sizeClasses): ?>
-											<div class="fixed-action-btn right <?= $sizeClasses ?> inline-fab commission-type-client-actions right">
-												<a class="btn-floating btn-large">
-													<i class="large material-icons">more_horiz</i>
-												</a>
-												<ul>
-													<?php if (in_array($commissionType->getId(), $_SESSION["user"]->getWishlistCommissionTypeIds())): ?>
-														<li class="tooltipped" data-tooltip="Remove from wishlist">
-															<a data-action="wishlist" data-state="on" class="btn-floating">
-																<i class="material-icons">star</i>
-															</a>
-														</li>
-													<?php else: ?>
-														<li class="tooltipped" data-tooltip="Add to wishlist">
-															<a data-action="wishlist" data-state="off" class="btn-floating">
-																<i class="material-icons">star_outline</i>
-															</a>
-														</li>
-													<?php endif; ?>
-													<li class="tooltipped" data-tooltip="Ask a Question">
-														<a href="<?= ROOTDIR ?>Message/New/<?= htmlspecialchars($commissionType->getArtistPage()->getPrecomposedMessageUrl("Commission Inquiry", 'Questions regarding "'.$commissionType->getName()."\"\n\n---\n\n")) ?>" class="btn-floating">
-															<i class="material-icons">contact_support</i>
-														</a>
-													</li>
-													<?php if ($commissionType->isAcceptingQuotes()): ?>
-														<li class="tooltipped" data-tooltip="Request a Quote">
-															<a class="btn-floating" href="<?= ROOTDIR ?>Quote/<?= $commissionType->getToken() ?>/">
-																<i class="material-icons">attach_money</i>
-															</a>
-														</li>
-													<?php endif; ?>
-													<?php if ($commissionType->isAcceptingRequests()): ?>
-														<li class="tooltipped" data-tooltip="Make a Request">
-															<a class="btn-floating" href="<?= ROOTDIR ?>Request/<?= $commissionType->getToken() ?>/">
-																<i class="material-icons">how_to_vote</i>
-															</a>
-														</li>
-													<?php endif; ?>
-													<?php if ($commissionType->isAcceptingTrades()): ?>
-														<li class="tooltipped" data-tooltip="Request a Trade">
-															<a class="btn-floating" href="<?= ROOTDIR ?>Trade/<?= $commissionType->getToken() ?>/">
-																<i class="material-icons">swap_horiz</i>
-															</a>
-														</li>
-													<?php endif; ?>
-													<?php if ($commissionType->isAcceptingCommissions()): ?>
-														<li class="tooltipped" data-tooltip="Commission">
-															<a class="btn-floating" href="<?= ROOTDIR ?>Commission/<?= $commissionType->getToken() ?>/">
-																<i class="material-icons">add_shopping_cart</i>
-															</a>
-														</li>
-													<?php endif; ?>
-												</ul>
-											</div>
-										<?php endforeach; ?>
-									<?php else: ?>
-										<div class="fixed-action-btn horizontal inline-fab commission-type-client-actions right tooltipped" data-position="left" data-tooltip="Please login">
-											<a class="btn-floating btn-large grey">
+						<div class="commission-type-row" id="ct-<?= htmlspecialchars($commissionType->getToken()) ?>" data-token="<?= htmlspecialchars($commissionType->getToken()) ?>">
+							<div class="commission-type-row-header">
+								<h5 class="inline-block"><?= htmlspecialchars($commissionType->getName()) ?></h5>
+								<?php if (User::isLoggedIn()): ?>
+									<?php foreach (["hide-on-small-only horizontal", "hide-on-med-and-up"] as $sizeClasses): ?>
+										<div class="fixed-action-btn right <?= $sizeClasses ?> inline-fab commission-type-client-actions right">
+											<a class="btn-floating btn-large">
 												<i class="large material-icons">more_horiz</i>
 											</a>
+											<ul>
+												<?php if (in_array($commissionType->getId(), $_SESSION["user"]->getWishlistCommissionTypeIds())): ?>
+													<li class="tooltipped" data-tooltip="Remove from wishlist">
+														<a data-action="wishlist" data-state="on" class="btn-floating">
+															<i class="material-icons">star</i>
+														</a>
+													</li>
+												<?php else: ?>
+													<li class="tooltipped" data-tooltip="Add to wishlist">
+														<a data-action="wishlist" data-state="off" class="btn-floating">
+															<i class="material-icons">star_outline</i>
+														</a>
+													</li>
+												<?php endif; ?>
+												<li class="tooltipped" data-tooltip="Ask a Question">
+													<a href="<?= ROOTDIR ?>Message/New/<?= htmlspecialchars($commissionType->getArtistPage()->getPrecomposedMessageUrl("Commission Inquiry", 'Questions regarding "'.$commissionType->getName()."\"\n\n---\n\n")) ?>" class="btn-floating">
+														<i class="material-icons">contact_support</i>
+													</a>
+												</li>
+												<?php if ($commissionType->isAcceptingQuotes()): ?>
+													<li class="tooltipped" data-tooltip="Request a Quote">
+														<a class="btn-floating" href="<?= ROOTDIR ?>Quote/<?= $commissionType->getToken() ?>/">
+															<i class="material-icons">attach_money</i>
+														</a>
+													</li>
+												<?php endif; ?>
+												<?php if ($commissionType->isAcceptingRequests()): ?>
+													<li class="tooltipped" data-tooltip="Make a Request">
+														<a class="btn-floating" href="<?= ROOTDIR ?>Request/<?= $commissionType->getToken() ?>/">
+															<i class="material-icons">how_to_vote</i>
+														</a>
+													</li>
+												<?php endif; ?>
+												<?php if ($commissionType->isAcceptingTrades()): ?>
+													<li class="tooltipped" data-tooltip="Request a Trade">
+														<a class="btn-floating" href="<?= ROOTDIR ?>Trade/<?= $commissionType->getToken() ?>/">
+															<i class="material-icons">swap_horiz</i>
+														</a>
+													</li>
+												<?php endif; ?>
+												<?php if ($commissionType->isAcceptingCommissions()): ?>
+													<li class="tooltipped" data-tooltip="Commission">
+														<a class="btn-floating" href="<?= ROOTDIR ?>Commission/<?= $commissionType->getToken() ?>/">
+															<i class="material-icons">add_shopping_cart</i>
+														</a>
+													</li>
+												<?php endif; ?>
+											</ul>
 										</div>
-									<?php endif; ?>
-								</div>
-								<p class="flow-text no-margin col s12">
-									<span class="tooltipped" data-tooltip="<?= htmlspecialchars($commissionType->getBaseUsdCost()) ?> USD">
-										<?= htmlspecialchars($commissionType->getBaseCost()) ?>
-									</span>
-								</p>
-								<p class="col s12">
-									<?= htmlspecialchars($commissionType->getBlurb()) ?>
-								</p>
+									<?php endforeach; ?>
+								<?php else: ?>
+									<div class="fixed-action-btn horizontal inline-fab commission-type-client-actions right tooltipped" data-position="left" data-tooltip="Please login">
+										<a class="btn-floating btn-large grey">
+											<i class="large material-icons">more_horiz</i>
+										</a>
+									</div>
+								<?php endif; ?>
 							</div>
+							<p class="flow-text no-margin col s12">
+								<span class="tooltipped" data-tooltip="<?= htmlspecialchars($commissionType->getBaseUsdCost()) ?> USD">
+									<?= htmlspecialchars($commissionType->getBaseCost()) ?>
+								</span>
+							</p>
+							<p class="col s12">
+								<?= htmlspecialchars($commissionType->getBlurb()) ?>
+							</p>
+						</div>
 						<?php $firstCommissionType = false; ?>
 					<?php endforeach; ?> 
 				<?php endif; ?>
