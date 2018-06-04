22a28aa905b59b21085627a971399644bd707484
Fauxil-Fox <fauxil@catalystapp.co>
2018-06-04 16:34:29 -0400

    add additional verbosity and hotfix webp thumbnail fallback

--- js/modules/webp_background_image_fallback.js
+++ js/modules/webp_background_image_fallback.js
@@ -1,7 +1,14 @@
 var img = new Image();
 img.onerror = function() {
-	$(".has-webp-thumb").each(function(a,b) {
-		$(b).attr("src", $(b).attr("primary-webp-thumb"));
+	window.log("webp", "webp support is not present", true);
+	$(".primary-webp-thumb").each(function(a,b) {
+		window.log("webp", "Reverting "+$(b).attr("src")+" to fallback "+$(b).attr("data-fallback-src"), true);
+		$(b).attr("src", $(b).attr("data-fallback-src"));
 	});
 };
+img.onload = function() {
+	window.log("webp", "webp support is present");
+};
+
+window.log("webp", "Testing webp support");
 img.src="data:image/webp;base64,UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA";
