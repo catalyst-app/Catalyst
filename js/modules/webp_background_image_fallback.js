88f887fff2b31f9e5b9fe58053908d52e847fc60
Fauxil-Fox <fauxil@catalystapp.co>
2018-06-04 21:44:56 -0400

    properly declare external css image

--- js/modules/webp_background_image_fallback.js
+++ js/modules/webp_background_image_fallback.js
@@ -3,7 +3,7 @@ img.onerror = function() {
 	window.log("webp", "webp support is not present", true);
 	$(".primary-webp-thumb").each(function(a,b) {
 		window.log("webp", "Reverting "+$(b).css("background-image")+" to fallback "+$(b).attr("data-fallback-src"), true);
-		$(b).css("background-image", $(b).attr("data-fallback-src"));
+		$(b).css("background-image", "url(\""+$(b).attr("data-fallback-src")+"\")");
 	});
 };
 img.onload = function() {
