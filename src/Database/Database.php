29e9f7b2717377d1dfdf8770097997b7b4f1790c
Fauxil-Fox <fauxil@catalystapp.co>
2018-06-24 18:05:53 -0400

    compartmentalize PDO thingies

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -35,13 +35,42 @@ class Database {
 		if (self::$dbh instanceof PDO) {
 			return;
 		}
-		self::$dbh = new PDO(self::DB_DRIVER.":host=".self::DB_SERVER.";port=".self::DB_PORT.";dbname=".self::DB_NAME.";charset=utf8mb4", self::DB_USER, self::DB_PASSWORD);
-		// raise errors
-		self::$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		self::$dbh = new PDO(self::getDataSourceName(), self::DB_USER, self::DB_PASSWORD, "utf8mb4");
+
+		foreach (self::getPdoAttributes() as $attr => $value) {
+			self::$dbh->setAttribute($attr, $value);
+		}
 
 		if (Controller::isDevelMode()) {
 			self::$dbh->query("set profiling=1");
 		}
+	}
+
+	/**
+	 * Get the DNS for PDO
+	 *
+	 * @return string
+	 */
+	public static function getDataSourceName() : string {
+		return self::DB_DRIVER.":".
+			"host=".self::DB_SERVER.";".
+			"port=".self::DB_PORT.";".
+			"dbname=".self::DB_NAME.";".
+			"charset=utf8mb4";
+	}
+
+	/**
+	 * Get attributes to apply to PDO connections
+	 *
+	 * @return array
+	 */
+	public static function getPdoAttributes() : array {
+		return [
+			// properly raise errors
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			// return as column => value,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		];
 	}
 
 	/**
