b91b50aa856bd49e0a249d235acb3960fc3bca60
Fauxil Fox <fauxil@catalystapp.co>
2019-03-27 16:04:07 -0400

    Make placeholder homepage go to something real

--- index.php
+++ index.php
@@ -4,24 +4,8 @@ define("ROOTDIR", "");
 define("REAL_ROOTDIR", "");
 
 require_once REAL_ROOTDIR."src/initializer.php";
-use \Catalyst\Page\{UniversalFunctions, Values};
-use \Catalyst\User\User;
+use \Catalyst\HTTPCode;
 
-define("PAGE_KEYWORD", Values::HOME[0]);
-define("PAGE_TITLE", Values::createTitle(Values::HOME[1], []));
-
-if (User::isLoggedIn()) {
-	define("PAGE_COLOR", $_SESSION["user"]->getColor());
-} else {
-	define("PAGE_COLOR", Values::DEFAULT_COLOR);
-}
-
-require_once Values::HEAD_INC;
-
-echo UniversalFunctions::createHeading("Home");
-?>
-			<div class="section">
-				<h3>"a less-samplier text" (not an error)</h3>
-			</div>
-<?php
-require_once Values::FOOTER_INC;
+HTTPCode::set(302);
+header("Location: ".ROOTDIR."About/", true);
+die("Redirecting...");
