793be92a19307caf5170deb927d374d7df2c3954
Fauxil Fox <fauxil@catalystapp.co>
2019-06-04 11:31:19 -0400

    Clean up TextField::getJsValidator

--- src/Form/Field/TextField.php
+++ src/Form/Field/TextField.php
@@ -133,11 +133,7 @@ class TextField extends AbstractField {
 	 * @return string The JS to validate the field
 	 */
 	public function getJsValidator() : string {
-		$str = '';
-
-		$str .= 'if (!(new window.formInputHandlers['.json_encode(self::class).'](document.getElementById('.json_encode($this->getId()).')).verify())) { return; };';
-
-		return $str;
+		return 'if (!(new window.formInputHandlers['.json_encode(self::class).'](document.getElementById('.json_encode($this->getId()).')).verify())) { return; }';
 	}
 
 	/**
